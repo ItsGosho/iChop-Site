a49cf41aabf17f8071696cefc8fa8607ad508000
Georgi Peev <itsgosho2@gmail.com>
2019-01-11 23:10:06 +0200

    Auth`s forms work🖌️
    1.Fixed the responsive issue that bothered me so much today and now the auth forms work good on mobile devices starting from width 284px!
    2.Renamed all important html elements with new simple convention [element-idea-location].
    3.Refactored the old garbage js code that looked awful with the repeating call to a element ,now its ok
    4.There is /to now only front-end/ validation on the register form including:
    -Username
    -Password
    -Confirm Password
    -Email
    !All of them of course following the back-end validation
    5.The next step is to finish the most of the auth`s logic like forgotten password,dynamic username validation check on register proceed with AJAX call and etc. + regroup the style of the auth`s forms in .css files
    6.Pushed the mentioned front-end changes to the back-end server

--- back-end/src/main/java/itsgosho/domain/models/binding/UserRegisterBindingModel.java
+++ back-end/src/main/java/itsgosho/domain/models/binding/UserRegisterBindingModel.java
@@ -1,6 +1,7 @@
 package itsgosho.domain.models.binding;
 
 import itsgosho.validators.fields.FieldsMustMatch;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
@@ -12,11 +13,14 @@ public class UserRegisterBindingModel {
 
     @NotNull
     @NotEmpty
+	@Length(min = 3)
+	@Length(max = 50)
     private String username;
 
     @NotNull
     @NotEmpty
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{6,}$")
+	@Length(max = 50)
     private String password;
 
     @NotNull
@@ -26,7 +30,8 @@ public class UserRegisterBindingModel {
     @NotNull
     @NotEmpty
     @Email(regexp = "(?:[a-z0-9!#$%&'*+\\=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+\\=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
-    private String email;
+    @Length(max = 50)
+	private String email;
 
     public String getUsername() {
         return username;
--- back-end/src/main/resources/static/js/auth/registerFormValidation.js
+++ back-end/src/main/resources/static/js/auth/registerFormValidation.js
@@ -1,31 +1,134 @@
 function runRegisterFormValidation() {
 
-    const EMAIL_PATTERN = "";
+    const EMAIL_PATTERN = "(?:[a-z0-9!#$%&'*+\\=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+\\=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+    const PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{6,}$";
 
-     //Validate Email:
-     let registerForm = $(`#emailRegisterForm`);
+    //Validate Username;
 
-     registerForm.on(`keypress keyup`,emailValidator);
+    let usernameField = $(`#input-username-registerForm`);
+    let errorUsernameLength = $(`#error-usernameIsShort-registerForm`);
 
-     function emailValidator(){
-          let val = registerForm.val();
-          let matcher = new RegExp("^([a-z0-9]{5,})$");
-          console.log(val);
-     }
+    usernameField.on(`keyup`, validateUsername);
 
-     //Show/Hide Password:
-    $(`#showPasswordRegisterForm`).on(`click`,showOrHidePassword);
-
-    function showOrHidePassword() {
-        if($(`#passwordRegisterForm`).attr(`type`)==='password'){
-            $(`#passwordRegisterForm`).attr(`type`,`text`);
-            $(`#confirmPasswordRegisterForm`).attr(`type`,`text`);
-
-            $(`#showPasswordRegisterForm`).text(`Hide`);
-        }else{
-            $(`#passwordRegisterForm`).attr(`type`,`password`);
-            $(`#confirmPasswordRegisterForm`).attr(`type`,`password`);
-            $(`#showPasswordRegisterForm`).text(`Show`);
+    function validateUsername() {
+        let val = usernameField.val();
+        if (val.length >= 3) {
+            usernameField.removeClass("border border-danger");
+            usernameField.addClass("border border-success");
+            errorUsernameLength.css(`display`, `none`);
+            return true;
+        } else {
+            errorUsernameLength.css(`display`, `inline-block`);
+            usernameField.addClass("border border-danger");
+            return false;
         }
     }
+
+
+    //Validate Passwords:
+    let showPasswordsButton = $(`#button-showPassword-registerForm`);
+    let passwordField = $(`#input-password-registerForm`);
+    let confirmPasswordField = $(`#input-confirmPassword-registerForm`);
+
+    passwordField.on(`keyup`, validatePasswordMatchPattern);
+    confirmPasswordField.on(`keyup`, validatePasswordEquals);
+
+
+    function validatePasswordEquals() {
+
+        if (!validatePasswordMatchPattern()) {
+            confirmPasswordField.addClass("border border-danger");
+            return false;
+        }
+
+        let passwordValue = passwordField.val();
+        let confirmPasswordValue = confirmPasswordField.val();
+
+        if (passwordValue === confirmPasswordValue) {
+
+            confirmPasswordField.removeClass("border border-danger");
+            confirmPasswordField.addClass("border border-success");
+            return true;
+        } else {
+            confirmPasswordField.addClass("border border-danger");
+            return false;
+        }
+    }
+
+    function validatePasswordMatchPattern() {
+        let val = passwordField.val();
+        let matcher = new RegExp(PASSWORD_PATTERN);
+
+        if (matcher.test(val)) {
+            passwordField.removeClass("border border-danger");
+            passwordField.addClass("border border-success");
+            return true;
+        } else {
+            passwordField.addClass("border border-danger");
+            return false;
+        }
+    }
+
+    let proceedRegisterButton = $(`#buttonSubmitRegister`);
+
+    proceedRegisterButton.on(`click`, function () {
+        //It is like that ,because if the methods are in the if method
+        //it will stop at the first `true` match and the other input fields
+        //will not be painted with the corresponding color :)
+
+        let resultUsername = validateUsername();
+        let resultPasswordEquals = validatePasswordEquals();
+        let resultPasswordMatchPattern = validatePasswordMatchPattern();
+        let resultEmail = validateEmail();
+
+        if (resultUsername &&
+            resultPasswordEquals &&
+            resultPasswordMatchPattern &&
+            resultEmail) {
+            return true;
+        }
+        return false;
+    });
+
+    //Show/Hide Passwords:
+    showPasswordsButton.on(`click`, showOrHidePassword);
+
+    function showOrHidePassword() {
+        if (passwordField.attr(`type`) === 'password') {
+            passwordField.attr(`type`, `text`);
+            confirmPasswordField.attr(`type`, `text`);
+
+            showPasswordsButton.text(`Hide`);
+        } else {
+            passwordField.attr(`type`, `password`);
+            confirmPasswordField.attr(`type`, `password`);
+            showPasswordsButton.text(`Show`);
+        }
+    }
+
+    //Validate Email:
+    let emailField = $(`#input-email-registerForm`);
+    emailField.on(`keyup`, validateEmail);
+
+    function validateEmail() {
+        let val = emailField.val();
+        let matcher = new RegExp(EMAIL_PATTERN);
+
+        if (matcher.test(val)) {
+            emailField.removeClass("border border-danger");
+            emailField.addClass("border border-success");
+            return true;
+        } else {
+            emailField.addClass("border border-danger");
+            return false;
+        }
+
+    }
+
+    //Password popover
+
+    $(document).ready(function () {
+        $('[data-toggle="popover"]').popover();
+    });
+
 }
