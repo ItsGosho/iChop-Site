69845e543dcc959837388e376e27a8b01c73a078
Georgi Peev <itsgosho2@gmail.com>
2019-10-01 12:08:22 +0300

    Added the Guest Navbar Component

---
+++ iChop - Front-End/front-end/src/components/navbar/roles/GuestNavbar.js
@@ -0,0 +1,55 @@
+import React,{Component} from 'react';
+
+class GuestNavbar extends Component {
+
+
+    render() {
+
+        return (
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+
+                </ul>
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item dropdown active">
+                        <div id="div-signIn-signInForms">
+                            <button id="button-signIn-navBar" type="button"
+                                    className="btn btn-success dropdown-toggle btn-sm"
+                                    data-toggle="dropdown"
+                                    aria-haspopup="true" aria-expanded="false">
+                                Sign in
+                            </button>
+                            <div id="div-authForms-signInForm" className="dropdown-menu dropdown-menu-right">
+
+                                <!--LOGINNN!!!-->
+
+                                <th:block th:insert="auth/login-dropdown.html"></th:block>
+
+                                <!--REGISTERR!!!-->
+
+                                <th:block th:insert="auth/register-dropdown.html"></th:block>
+
+                                <!--FORGOTTEN PASSWORD!!!-->
+
+                                <th:block th:insert="auth/forgotten_password-dropdown.html"></th:block>
+
+
+                                <!--SCRIPT!!!-->
+                                <script src="/res/js/auth/authFormsConfig.js"></script>
+                                <script src="/res/js/auth/url/authUrlHandlers.js"></script>
+                                <script>
+
+                                    runAuthFormsConfig();
+                                    runAuthUrlHandlers();
+                                </script>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+
+}
+
+export default GuestNavbar;
