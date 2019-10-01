1f46c6ef2494d6402e8ae0c8e965caa2ffd01243
Georgi Peev <itsgosho2@gmail.com>
2019-10-01 20:13:25 +0300

    Added all Routing Urls that will be used

--- iChop - Front-End/front-end/src/constants/routing.constants.js
+++ iChop - Front-End/front-end/src/constants/routing.constants.js
@@ -1,23 +1,70 @@
 const RoutingURLs = {
     HOME: '/',
 
+    PLAYER: {
+        ACCOUNT: {
+            LINK: '/player/account/link' /*User+*/
+        },
+        PROFILE: {
+            VIEW: '/player/:uuid/profile' /*Everyone*/
+        }
+    },
+
     AUTHENTICATION: {
-        LOGOUT: '/logout'
+        LOGOUT: '/auth/logout', /*Logged in*/
+        RESET: {
+            PASSWORD: '/auth/reset/password' /*Logged out*/
+        }
     },
 
     USER: {
-        PROFILE: '/user/:username/profile',
-        ALL: '/user/all',
-        PROFILE_OPTIONS_INFORMATION: '/user/my-profile/options/information',
+
+        PROFILE: {
+            VIEW: '/user/:username/profile' /*Everyone*/
+        },
+
+        ALL: '/users/all', /*Admin+*/
+
+        FOLLOWERS: {
+            ALL: '/user/:username/followers/all' /*Everyone+*/
+        },
+
+        FOLLOWINGS: {
+            ALL: '/user/:username/followings/all' /*Everyone+*/
+        },
+
+        CONTROL_PANEL: {
+            Role: '/user/:username/control/role' /*Admin+*/
+        },
+
+        OPTIONS: {
+            INFORMATION: '/user/:username/options/information', /*User+*/
+            PASSWORD: '/user/:username/options/password', /*User+*/
+            MINECRAFT: '/user/:username/options/minecraft', /*User+*/
+        },
+
+        /*TODO: move it out from here*/
         AVATAR: 'http://localhost:8001/data/user/:username/avatar'
     },
 
-    THREAD: {
-        CREATE: '/thread/create'
+    POST: {
+        REPORT: {
+            ALL: '/post/reports/all' /*Moderator+*/
+        }
     },
 
     COMMENT: {
-        REPORTS_ALL: '/comment/reports/all'
+        REPORT: {
+            ALL: '/comment/reports/all' /*Moderator+*/
+        }
+    },
+
+    THREAD: {
+        CREATE: '/thread/create', /*Moderator+*/
+        VIEW: '/thread/:id/view', /*Everyone*/
+        REPORT: {
+            ALL: '/thread/reports/all' /*Moderator+*/
+        }
     },
 };
 
