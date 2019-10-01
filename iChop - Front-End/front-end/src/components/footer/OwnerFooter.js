a7208c11b7a6b901719978371f77b2c1dcb0915a
Georgi Peev <itsgosho2@gmail.com>
2019-10-01 11:32:48 +0300

    Owner footer added

---
+++ iChop - Front-End/front-end/src/components/footer/OwnerFooter.js
@@ -0,0 +1,66 @@
+import React, {Component, Fragment} from 'react';
+
+class OwnerFooter extends Component {
+
+
+    render() {
+
+        return (
+            <Fragment>
+                <div className="col-md-2 mx-auto">
+
+                    <h5 className="font-weight-bold text-uppercase mt-3 mb-4">{---}</h5>
+
+                    <ul className="list-unstyled">
+                        <li>
+                            <a th:href="@{'/user/'+${#authentication.name}+'/profile'}">
+                                <small>👤</small>
+                                <span>Profile</span></a>
+                        </li>
+                        <li>
+                            <a href="/user/my-profile/options/information">
+                                <small>⚙</small>
+                                <span>Options</span></a>
+                        </li>
+                        <li>
+                            <a href="/thread/create">
+                                <small>🚩</small>
+                                Create Thread</a>
+                        </li>
+                    </ul>
+
+                </div>
+
+                <hr className="clearfix w-100 d-md-none">
+
+                    <div className="col-md-2 mx-auto">
+
+                        <h5 className="font-weight-bold text-uppercase mt-3 mb-4">{---}</h5>
+
+                        <ul className="list-unstyled">
+                            <li>
+                                <a href="/user/all">
+                                    <small>👥</small>
+                                    Users</a>
+                            </li>
+                            <li>
+                                <a href="/comment/reports/all">
+                                    <small>⚠</small>
+                                    Reports</a>
+                            </li>
+
+                            <li>
+                                <a href="/logout">
+                                    <small>🚪</small>
+                                    Logout</a>
+                            </li>
+                        </ul>
+
+                    </div>
+            </Fragment>
+    );
+    }
+
+    }
+
+    export default OwnerFooter;
