fa131ecb83656e8985c79d9c8a12d2233ffbccfc
Georgi Peev <itsgosho2@gmail.com>
2019-10-01 15:13:14 +0300

    Added the store and the provider in index.js with thunk,logger and redux devtools middlewares

--- iChop - Front-End/front-end/src/index.js
+++ iChop - Front-End/front-end/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
+import reducers from './redux/reducers'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import Provider from "react-redux/es/components/Provider";
 
-let store = '';
+let store = createStore(
+    combineReducers(reducers),
+    compose(applyMiddleware(thunk, logger), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root'));
