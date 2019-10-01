f69e7dd965b56fa1153cbb5a4f20be8c3a5530cf
Georgi Peev <itsgosho2@gmail.com>
2019-10-01 12:29:38 +0300

    Added Owener Navbar`s RoutingURLs

--- iChop - Front-End/front-end/src/components/navbar/roles/OwnerNavbar.js
+++ iChop - Front-End/front-end/src/components/navbar/roles/OwnerNavbar.js
@@ -1,9 +1,14 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
+import RoutingURLs from "../../../constants/routing.constants";
+import {Link} from "react-router-dom";
 
 class OwnerNavbar extends Component {
 
 
     render() {
+        let username = '';
+        let avatarUrl = RoutingURLs.USER.AVATAR.replace(':username', username);
+        let profileUrl = RoutingURLs.USER.PROFILE.replace(':username', username);
 
         return (
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -17,31 +22,31 @@ class OwnerNavbar extends Component {
                                     data-toggle="dropdown"
                                     aria-haspopup="true" aria-expanded="false">
                                 <img
-                                    th:src="@{http://localhost:8001/data/user/{username}/avatar(username=${#authentication.getName()})}"
+                                    src={avatarUrl}
                                     onError="this.onerror = null;this.src = '/res/img/avatar-user.png'"
                                     className="img-user_avatar-top_nav_bar"/>
-                                    <span>⚙</span>
+                                <span>⚙</span>
                             </button>
                             <div className="dropdown-menu dropdown-menu-right">
-                                <a className="dropdown-item" th:href="@{'/user/'+${#authentication.name}+'/profile'}">
+                                <Link className="dropdown-item" to={profileUrl}>
                                     <small>👤</small>
-                                    <span>Profile</span></a>
-                                <a className="dropdown-item" href="/user/my-profile/options/information">
+                                    <span>Profile</span></Link>
+                                <Link className="dropdown-item" to={RoutingURLs.USER.PROFILE_OPTIONS_INFORMATION}>
                                     <small>⚙</small>
-                                    <span>Options</span></a>
-                                <a className="dropdown-item" href="/thread/create">
+                                    <span>Options</span></Link>
+                                <Link className="dropdown-item" to={RoutingURLs.THREAD.CREATE}>
                                     <small>🚩</small>
-                                    Create Thread</a>
-                                <a className="dropdown-item" href="/user/all">
+                                    Create Thread</Link>
+                                <Link className="dropdown-item" to={RoutingURLs.USER.ALL}>
                                     <small>👥</small>
-                                    All Users</a>
-                                <a className="dropdown-item" href="/comment/reports/all">
+                                    All Users</Link>
+                                <Link className="dropdown-item" to={RoutingURLs.COMMENT.REPORTS_ALL}>
                                     <small>⚠</small>
-                                    Reports</a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="/logout">
+                                    Reports</Link>
+                                <div className="dropdown-divider"/>
+                                <Link className="dropdown-item" to={RoutingURLs.AUTHENTICATION.LOGOUT}>
                                     <small>🚪</small>
-                                    Logout</a>
+                                    Logout</Link>
                             </div>
                         </div>
                     </li>
