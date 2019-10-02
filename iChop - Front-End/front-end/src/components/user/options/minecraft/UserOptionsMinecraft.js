6fb93c7d223ab746316da78b04e054232966bc5a
Georgi Peev <itsgosho2@gmail.com>
2019-10-02 18:36:04 +0300

    User Options Component Finished!

--- iChop - Front-End/front-end/src/components/user/options/minecraft/UserOptionsMinecraft.js
+++ iChop - Front-End/front-end/src/components/user/options/minecraft/UserOptionsMinecraft.js
@@ -1,10 +1,17 @@
 import React, {Component, Fragment} from 'react';
+import RoutingURLs from "../../../../constants/routing.constants";
+import {Link} from "react-router-dom";
+import ServerRoutingURLs from "../../../../constants/server.routing.urls";
 
 class UserOptionsMinecraft extends Component {
 
 
     render() {
-        let isAccountLinked = false;
+        let isAccountLinked = true;
+        let playerName = 'ItsGosho';
+        let uuid = '8ed20904-3262-401a-901a-1946504d2eea';
+        let accountProfile = RoutingURLs.PLAYER.PROFILE.VIEW.replace(':uuid', uuid);
+        let crafatarSkinUrl = ServerRoutingURLs.OUTSIDE.CRAFATAR.MINECRAFT.SKIN.replace(':uuid', uuid);
 
         return (
             <Fragment>
@@ -26,11 +33,11 @@ class UserOptionsMinecraft extends Component {
 
                                     <div className="row" align="center">
                                         <div className="col-lg">
-                                        <span>To link your account you must be logged in the site with the profile that you want to link your account
-                                        ,then in the game type <span><b>/linkaccount</b></span> ,then a <span><b>link</b></span> will be generated ,click
-                                            it and a page <span><b>in the site</b></span> will open
-                                        with confirmation to link your account.Note that if you link your account you cant link another until <span><b>the
-                                        previous is unlinked</b></span>.</span>
+                                           <span>To link your account you must be logged in the site with the profile that you want to link your account
+                                           ,then in the game type <span><b>/linkaccount</b></span> ,then a <span><b>link</b></span> will be generated ,click
+                                               it and a page <span><b>in the site</b></span> will open
+                                           with confirmation to link your account.Note that if you link your account you cant link another until <span><b>the
+                                           previous is unlinked</b></span>.</span>
                                         </div>
                                     </div>
 
@@ -42,25 +49,24 @@ class UserOptionsMinecraft extends Component {
                                 <Fragment>
                                     <div className="row" align="center">
                                         <div className="col-lg">
-                                            <span><b><a th:href="@{/player/{uuid}(uuid=*{playerUUID})}"
-                                                        th:text="*{playerName}">Player Name</a></b></span>
+                                            <span>
+                                                <b>
+                                                    <Link to={accountProfile}>{playerName}</Link>
+                                                </b>
+                                            </span>
                                         </div>
                                     </div>
 
                                     <div className="dropdown-divider"/>
 
                                     <div align="center">
-                                        <img th:src="@{https://crafatar.com/renders/body/{uuid}(uuid=*{playerUUID})}"
-                                             style="width: 95px;height: 185px"/>
+                                        <img src={crafatarSkinUrl} style={{'width': '95px', 'height': '200px'}} alt=''/>
                                     </div>
 
                                     <div className="dropdown-divider"/>
 
-                                    <div align="center" style="margin-top: 10px">
-                                        <form style="all:initial" method="post"
-                                              action="/user/my-profile/options/minecraft/unlink">
-                                            <button type="submit" className="btn btn-warning">Unlink</button>
-                                        </form>
+                                    <div align="center" style={{'marginTop': '10px'}}>
+                                        <button type="button" className="btn btn-warning">Unlink</button>
                                     </div>
                                 </Fragment>
                             );
--- iChop - Front-End/front-end/src/constants/server.routing.urls.js
+++ iChop - Front-End/front-end/src/constants/server.routing.urls.js
@@ -1,5 +1,6 @@
 const CORE_SERVER_DOMAIN = 'http://localhost:8000';
 const DATA_SERVER_DOMAIN = 'http://localhost:8001';
+const OTHER_CRAFATAR_SERVER_DOMAIN = 'https://crafatar.com';
 
 const ServerRoutingURLs = {
     CORE: {},
@@ -7,6 +8,13 @@ const ServerRoutingURLs = {
         USER: {
             AVATAR: {
                 GET: `${DATA_SERVER_DOMAIN}/data/user/:username/avatar`
+            }
+        }
+    },
+    OUTSIDE: {
+        CRAFATAR: {
+            MINECRAFT: {
+                SKIN: `${OTHER_CRAFATAR_SERVER_DOMAIN}/renders/body/:uuid`
             }
         }
     }
