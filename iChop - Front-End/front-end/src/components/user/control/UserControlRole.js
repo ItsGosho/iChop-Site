aa8756a55dbd95fbffcfc69ec96d6f720fc8c649
Georgi Peev <itsgosho2@gmail.com>
2019-10-02 16:52:55 +0300

    Fixed the styling of User Control Role and the expected functionality

--- iChop - Front-End/front-end/src/components/user/control/UserControlRole.js
+++ iChop - Front-End/front-end/src/components/user/control/UserControlRole.js
@@ -6,12 +6,14 @@ class UserControlRole extends Component {
 
 
     render() {
+        let previousRole = null;
         let role = Roles.USER;
+        let nextRole = Roles.MODERATOR;
+
         let logs = [
             {happenedOn: dateFormat(Date.now(), "dd mmmm,yyyy HH:mm"), message: ' Changed the role to X'},
             {happenedOn: dateFormat(Date.now(), "dd mmmm,yyyy HH:mm"), message: ' Changed the role to X'},
         ];
-
 
         return (
             <div>
@@ -21,49 +23,64 @@ class UserControlRole extends Component {
                     </div>
                 </div>
                 <div className="row">
-                    {/*<div className="col-md-auto">
-                        <th:block th:if="*{role != 'OWNER'}">
-                            <th:block th:if="*{previousRole != null}">
+                    <div className="col-md-auto">
 
-                                <form style="all:initial;font-family: Consolas" method="post"
-                                      th:action="@{/user/{username}/control/role-management/demote(username=*{username})}">
-                                    <button type="submit" className="btn btn-warning btn-sm"><span>👇🏻</span><span
-                                        th:text="*{previousRole}"></span></button>
-                                </form>
+                        {
+                            (() => {
+                                if (role !== Roles.OWNER) {
 
-                            </th:block>
-                            <th:block th:if="*{nextRole != null and nextRole != 'OWNER'}">
-
-                                <form style="all:initial;font-family: Consolas" method="post"
-                                      th:action="@{/user/{username}/control/role-management/promote(username=*{username})}">
-                                    <button type="submit" className="btn btn-warning btn-sm"><span>👆🏻</span><span
-                                        th:text="*{nextRole}"></span></button>
-                                </form>
-
-                            </th:block>
-                        </th:block>
-                    </div>*/}
-                    <div className="row">
-                        <div className="col-md-auto">
-                            <small><b>Logs:</b></small>
-                        </div>
-                        <div className="col-md-auto">
-                            <ul>
-
-                                {
-                                    logs.map((item, index) => {
+                                    if (previousRole !== null) {
                                         return (
-                                            <li key={index}>
-                                                <small>
-                                                    <b>[{item.happenedOn}]</b>
-                                                    <span>{item.message}</span>.
-                                                </small>
-                                            </li>
+                                            <button type="button" className="btn btn-warning btn-sm" style={{'marginRight':'10px'}}>
+                                                <span>👇🏻</span><span>{previousRole}</span>
+                                            </button>
                                         );
-                                    })
+                                    }
                                 }
-                            </ul>
-                        </div>
+
+                                return null;
+                            })()
+                        }
+
+                        {
+                            (() => {
+                                if (role !== Roles.OWNER) {
+
+                                    if (nextRole !== null && nextRole !== Roles.OWNER) {
+                                        return (
+                                            <button type="button" className="btn btn-warning btn-sm" style={{'marginRight':'10px'}}>
+                                                <span>👆🏻</span>
+                                                <span>{nextRole}</span>
+                                            </button>
+                                        );
+                                    }
+                                }
+
+                                return null;
+                            })()
+                        }
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-auto">
+                        <small><b>Logs:</b></small>
+                    </div>
+                    <div className="col-md-auto">
+                        <ul>
+
+                            {
+                                logs.map((item, index) => {
+                                    return (
+                                        <li key={index}>
+                                            <small>
+                                                <b>[{item.happenedOn}]</b>
+                                                <span>{item.message}</span>.
+                                            </small>
+                                        </li>
+                                    );
+                                })
+                            }
+                        </ul>
                     </div>
                 </div>
             </div>
