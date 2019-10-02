9b4a6fa81dcb7b0af9a2212130b1da801a05a541
Georgi Peev <itsgosho2@gmail.com>
2019-10-02 19:49:01 +0300

    Added Form Hoc to handle inputs in GuestLoginDropdown

--- iChop - Front-End/front-end/src/components/navbar/roles/guest/GuestLoginDropdown.js
+++ iChop - Front-End/front-end/src/components/navbar/roles/guest/GuestLoginDropdown.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
 import navbarGuestReduxHoc from "../../../../redux/hocs/navbar.guest.hoc";
+import formHoc from "../../../../hocs/form.hoc";
 
 class GuestLoginDropdown extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.onLogin = this.onLogin.bind(this);
+    }
+
+    onLogin() {
+        let {usernameOrEmail, password} = this.props.formData;
+
+        console.log(usernameOrEmail);
+        console.log(password);
+    }
 
     render() {
+        let {onChange} = this.props.formMethods;
+
         return (
             <div id="div-login-dropdown">
                 <form className="px-4 py-3">
+
                     <div className="form-group" id="formGroup-usernameOrEmail-loginForm">
                         <div className="input-group mb-2">
                             <div className="input-group-prepend">
@@ -15,9 +31,10 @@ class GuestLoginDropdown extends Component {
                             </div>
                             <input type="text" className="form-control" id="input-usernameOrEmail-loginForm"
                                    autoComplete="off"
-                                   name="usernameOrEmail" placeholder="Username or Email..."/>
+                                   name="usernameOrEmail" placeholder="Username or Email..." onChange={onChange}/>
                         </div>
                     </div>
+
                     <div className="form-group">
                         <div className="input-group mb-2">
                             <div className="input-group-prepend">
@@ -25,11 +42,12 @@ class GuestLoginDropdown extends Component {
                             </div>
                             <input type="password" className="form-control" id="input-password-loginForm"
                                    autoComplete="off"
-                                   name="password" placeholder="Password..."/>
+                                   name="password" placeholder="Password..." onChange={onChange}/>
                         </div>
                     </div>
-                    <small id="error-invalidCredentials-loginForm">⚡Invalid credentials :/
-                    </small>
+
+                    <small id="error-invalidCredentials-loginForm">⚡Invalid credentials :/</small>
+
                     <div className="custom-control custom-checkbox">
                         <input type="checkbox" name="rememberMe" className="custom-control-input"
                                id="input-rememberMe-loginForm"/>
@@ -40,14 +58,17 @@ class GuestLoginDropdown extends Component {
 
                     <input type="hidden" id="input-user_location-loginDropdown" name="userLocation"/>
 
-                    <button type="button" data-style="zoom-in" className="btn btn-primary">Login</button>
+                    <button type="button" data-style="zoom-in" className="btn btn-primary" onClick={this.onLogin}>Login</button>
+
                     <div className="dropdown-divider"/>
                 </form>
+
                 <a id="button-goToRegister-loginForm" className="dropdown-item">
                     <button type="button" onClick={this.props.selectRegister}
                             className="btn btn-success btn-sm">Register
                     </button>
                 </a>
+
                 <a id="button-goToForgottenPassword-loginForm" className="dropdown-item">
                     <button className="btn btn-warning btn-sm" onClick={this.props.selectForgottenPassword}>Forgotten
                         Password
@@ -59,4 +80,4 @@ class GuestLoginDropdown extends Component {
 
 }
 
-export default navbarGuestReduxHoc(GuestLoginDropdown);
+export default formHoc(navbarGuestReduxHoc(GuestLoginDropdown));
