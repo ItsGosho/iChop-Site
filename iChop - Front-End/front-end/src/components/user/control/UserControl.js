4789ff7378759c33329f0464ddbecb780abcf613
Georgi Peev <itsgosho2@gmail.com>
2019-10-02 17:45:10 +0300

    Added User Options Component with inital template ,which needs to be refactored

--- iChop - Front-End/front-end/src/components/user/control/UserControl.js
+++ iChop - Front-End/front-end/src/components/user/control/UserControl.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import UserControlInformation from "./UserControlInformation";
+import UserControlInformation from "./information/UserControlInformation";
 import UserControlRole from "./role/UserControlRole";
 import controlSidebarReduxHoc from "../../../redux/hocs/control.sidebar.hoc";
 import UserControlNav from "./base/UserControlNav";
---
+++ iChop - Front-End/front-end/src/components/user/options/UserOptions.js
@@ -0,0 +1,43 @@
+import React,{Component} from 'react';
+
+class UserOptions extends Component {
+
+
+    render() {
+
+        return (
+            <div className="container" style="margin-left: 0">
+                <div className="row">
+                    <div className="col-sm">
+                        <div className="card" style="width: 15rem;">
+                            <div className="card-header">
+                                Options Menu
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                <a href="/user/my-profile/options/information">
+                                    <li className="list-group-item control-option">Information</li>
+                                </a>
+                                <a href="/user/my-profile/options/change-password">
+                                    <li className="list-group-item control-option">Change Password</li>
+                                </a>
+                                <a href="/user/my-profile/options/minecraft">
+                                    <li className="list-group-item control-option">Minecraft</li>
+                                </a>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-sm">
+                        <div className="card">
+                            <div className="card-body">
+                                <th:block th:insert="${optionsPage}"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default UserOptions;
