d59798edaf82f2df1ef23703d284a207afcea943
itsgosho <itsgosho2@gmail.com>
2019-10-05 00:28:14 +0300

    Added onUserAvatar error for the picture in user profile

--- iChop - Front-End/front-end/src/components/user/profile/UserProfileLeftSideInformation.js
+++ iChop - Front-End/front-end/src/components/user/profile/UserProfileLeftSideInformation.js
@@ -6,22 +6,34 @@ import SideInformationJoinedOn from "./side/SideInformationJoinedOn";
 import SideInformationMessages from "./side/SideInformationMessages";
 import SideInformationTotalLikes from "./side/SideInformationTotalLikes";
 import SideInformationTotalDislikes from "./side/SideInformationTotalDislikes";
+import SideInformationLocation from "./side/SideInformationLocation";
+import FrontEndResourcesRoutingURLs from "../../../constants/front-end.resources.routings";
 
 class UserProfileLeftSideInformation extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.onUserAvatarError = this.onUserAvatarError.bind(this);
+    }
+
+    onUserAvatarError(event) {
+        event.target.onerror = null;
+        event.target.src = FrontEndResourcesRoutingURLs.USER.AVATAR;
+    }
 
     render() {
         let username = 'ItsGosho';
         let userAvatarUrl = ServerRoutingURLs.DATA.USER.AVATAR.GET.replace(':username', username);
-        let lastOnline = new Date();
 
         return (
             <div className="col-md-auto">
                 <div className="col-md-sm user-info">
 
                     <div align="center">
-                        <img th:src="@{http://localhost:8001/data/user/{username}/avatar(username=*{username})}"
-                             onError="this.onerror = null;this.src = '/res/img/avatar-user.png'"
+                        <img src={userAvatarUrl}
+                             onError={this.onUserAvatarError}
+                             alt=''
                              className="img-user-avatar"/>
                     </div>
 
@@ -99,8 +111,7 @@ class UserProfileLeftSideInformation extends Component {
                         <div className="card-body"
                              style="line-height: 13px;font-family: Consolas;margin-left: -20px;margin-bottom: -15px;margin-top: -15px">
 
-
-
+                            <SideInformationLocation/>
 
                     </div>
 
