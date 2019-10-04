308909897cc4013e98f471fb58b214eeb1a52890
itsgosho <itsgosho2@gmail.com>
2019-10-05 00:54:45 +0300

    Refactored the followings component

--- iChop - Front-End/front-end/src/components/user/profile/side/SideInformationFollowings.js
+++ iChop - Front-End/front-end/src/components/user/profile/side/SideInformationFollowings.js
@@ -1,24 +1,57 @@
 import React,{Component} from 'react';
+import FrontEndResourcesRoutingURLs from "../../../../constants/front-end.resources.routings";
+import RoutingURLs from "../../../../constants/routing.constants";
+import ServerRoutingURLs from "../../../../constants/server.routing.urls";
+import {Link} from "react-router-dom";
 
 class SideInformationFollowings extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.onUserAvatarError = this.onUserAvatarError.bind(this);
+    }
+
+
+    onUserAvatarError(event) {
+        event.target.onerror = null;
+        event.target.src = FrontEndResourcesRoutingURLs.USER.AVATAR;
+    }
+
     render() {
+        let followings = [].slice(0, 4);
 
         return (
             <div className="row d-flex justify-content-center align-items-center"
-                 style="width: 100%;margin-left: 0px">
-                <th:block th:each="following : *{followings}" th:object="${following}">
-                    <th:block th:if="${followingStat.index < 4}">
-                        <a th:href="@{/user/{username}/profile(username=*{username})}">
-                            <img
-                                th:src="@{http://localhost:8001/data/user/{username}/avatar(username=*{username})}"
-                                className="img-user-avatar"
-                                th:title="*{username}"
-                                onError="this.onerror = null;this.src = '/res/img/avatar-user.png'"
-                                style="width: 30px;height: 30px;margin-left: 5px;margin-top: 2px;margin-bottom: 2px">
-                        </a>
-                    </th:block>
-                </th:block>
+                 style={{'width': '100%', 'margin-left': '0px'}}>
+
+                {
+                    (() => {
+                        followings.map((following, index) => {
+                            let {username} = following;
+                            let profileUrl = RoutingURLs.USER.PROFILE.VIEW.replace(':username', username);
+                            let avatarUrl = ServerRoutingURLs.DATA.USER.AVATAR.GET.replace(':username', username);
+
+                            return (
+                                <Link to={profileUrl}>
+                                    <img key={index}
+                                         src={avatarUrl}
+                                         className="img-user-avatar"
+                                         title={username}
+                                         onError={this.onUserAvatarError}
+                                         alt=''
+                                         style={{
+                                             'width': '30px',
+                                             'height': '30px',
+                                             'marginLeft': '5px',
+                                             'marginTop': '2px',
+                                             'marginBottom': '2px'
+                                         }}/>
+                                </Link>
+                            );
+                        })
+                    })()
+                }
             </div>
         );
     }
