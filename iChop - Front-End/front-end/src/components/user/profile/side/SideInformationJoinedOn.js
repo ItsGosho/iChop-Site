a4802fc9cda3812338a76e08a20bc15de91dcf95
itsgosho <itsgosho2@gmail.com>
2019-10-05 00:58:07 +0300

    Finished refactoring the join on side component information

--- iChop - Front-End/front-end/src/components/user/profile/side/SideInformationJoinedOn.js
+++ iChop - Front-End/front-end/src/components/user/profile/side/SideInformationJoinedOn.js
@@ -1,9 +1,10 @@
 import React,{Component} from 'react';
+import dateFormat from 'dateformat';
 
 class SideInformationJoinedOn extends Component {
 
     render() {
-
+         let registrationDate = new Date();
 
         return (
             <div className="col-md-auto">
@@ -13,9 +14,8 @@ class SideInformationJoinedOn extends Component {
                     </div>
                     <div className="col-md-auto" style={{'columnWidth': '100px'}}>
                                <span style={{'display': 'inlineBlock','float': 'right'}}
-                                     th:text="*{#temporals.format(registrationDate, 'MMM dd,yyyy')}"
-                                     th:title="*{#temporals.format(registrationDate, 'HH:mm')}">
-                                    Unknown 00,0000
+                                     title={dateFormat(registrationDate,'HH:mm')}>
+                                   {dateFormat(registrationDate,'MMM dd,yyyy')}
                                </span>
                     </div>
                 </div>
