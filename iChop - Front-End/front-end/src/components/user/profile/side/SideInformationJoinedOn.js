39ada5d8af697feb6e4823c6440e3c02fa8519f7
itsgosho <itsgosho2@gmail.com>
2019-10-05 00:56:16 +0300

    Fixed the style in join on information

--- iChop - Front-End/front-end/src/components/user/profile/side/SideInformationJoinedOn.js
+++ iChop - Front-End/front-end/src/components/user/profile/side/SideInformationJoinedOn.js
@@ -4,14 +4,15 @@ class SideInformationJoinedOn extends Component {
 
     render() {
 
+
         return (
             <div className="col-md-auto">
                 <div className="row user-base-info-registration_date-title">
                     <div className="col-md-auto">
                         Joined on:
                     </div>
-                    <div className="col-md-auto" style="column-width: 100px">
-                               <span style="display: inline-block;float: right;"
+                    <div className="col-md-auto" style={{'columnWidth': '100px'}}>
+                               <span style={{'display': 'inlineBlock','float': 'right'}}
                                      th:text="*{#temporals.format(registrationDate, 'MMM dd,yyyy')}"
                                      th:title="*{#temporals.format(registrationDate, 'HH:mm')}">
                                     Unknown 00,0000
