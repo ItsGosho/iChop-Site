e5178cfbfeb5e566d2d1445fec64ddb618972994
itsgosho <itsgosho2@gmail.com>
2019-10-06 01:07:52 +0300

    Moved the text editor`s font picker into separate component

--- iChop - Front-End/front-end/src/components/editor/TextEditor.js
+++ iChop - Front-End/front-end/src/components/editor/TextEditor.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import TextEditorColorPicker from "./TextEditorColorPicker";
 import TextEditorFontSizePicker from "./TextEditorFontSizePicker";
+import TextEditorFontPicker from "./TextEditorFontPicker";
 
 class TextEditor extends Component {
 
@@ -30,37 +31,7 @@ class TextEditor extends Component {
 
                 <TextEditorFontSizePicker/>
 
-                                        <a href="#" id="button-fontFamily-textEditor" className="dropdown-toggle"
-                                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                                           title="Font Family" onClick={this.preventDefault}><i
-                                            className="material-icons">font_download</i></a>
-                                        <ul className="dropdown-menu scrollable-menu" role="menu"
-                                            style={{'minHeight': '100px', 'maxHeight': '100px', 'overflow': 'auto'}}>
-                                        <a id="button-fontFamily_arial-textEditor" data-content="Arial" href="#"
-                                           onClick={this.preventDefault}><li style={{'fontFamily': 'Arial'}}>Arial</li></a>
-                                        <a id="button-fontFamily_bookAntiqua-textEditor" data-content="Book Antiqua"
-                                           href="#" onClick={this.preventDefault}><li
-                                            style={{'fontFamily': 'Book Antiqua'}}>Book Antiqua</li></a>
-                                        <a id="button-fontFamily_courierNew-textEditor" data-content="Courier New"
-                                           href="#" onClick={this.preventDefault}><li
-                                            style={{'fontFamily': 'Courier New'}}>Courier New</li></a>
-                                        <a id="button-fontFamily_georgia-textEditor" data-content="Georgia" href="#"
-                                           onClick={this.preventDefault}><li
-                                            style={{'fontFamily': 'Georgia'}}>Georgia</li></a>
-                                        <a id="button-fontFamily_tahoma-textEditor" data-content="Tahoma" href="#"
-                                           onClick={this.preventDefault}><li
-                                            style={{'fontFamily': 'Tahoma'}}>Tahoma</li></a>
-                                        <a id="button-fontFamily_timesNewRoman-textEditor"
-                                           data-content="Times New Roman" href="#" onClick={this.preventDefault}><li
-                                            style={{'fontFamily': 'Times New Roman'}}>Times New Roman</li></a>
-                                        <a id="button-fontFamily_trebuchetMS-textEditor" data-content="Trebuchet MS"
-                                           href="#" onClick={this.preventDefault}><li
-                                            style={{'fontFamily': 'Trebuchet MS'}}>Trebuchet MS</li></a>
-                                        <a id="button-fontFamily_verdana-textEditor" data-content="Verdana" href="#"
-                                           onClick={this.preventDefault}><li
-                                            style={{'fontFamily': 'Verdana'}}>Verdana</li></a>
-                                    </ul>
-                                </span>
+                <TextEditorFontPicker/>
 
                 <a href="#" id="button-insertLink-textEditor" data-toggle="modal"
                    data-target="#modal-insertLink-textEditor"
