dc57856806a078754eb77ea1d4e597fdf4ea2476
itsgosho <itsgosho2@gmail.com>
2019-10-05 11:40:06 +0300

    added iteration for posts in pane all posts

--- iChop - Front-End/front-end/src/components/user/profile/panes/posts/PaneAllPosts.js
+++ iChop - Front-End/front-end/src/components/user/profile/panes/posts/PaneAllPosts.js
@@ -1,48 +1,58 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import PanePostActions from "./PanePostActions";
 
 class PaneAllPosts extends Component {
 
     render() {
         let isAuthenticated = true;
+        let posts = [];
 
         return (
-        <div className="card post" style="margin-top: 10px">
-            <div className="card-body" style="margin-bottom: -15px">
+            <Fragment>
+            {
+                (() => {
+                    posts.map((post,index)=>{
+                        return (
+                            <div className="card post" style={{'marginTop': '10px'}}>
+                                <div className="card-body" style={{'marginBottom': '-15px'}}>
 
-                <div className="row" style="margin-top: -15px">
-                    <div className="col-md-1" style="margin-top: 9px">
-                        <img
-                            th:src="@{http://localhost:8001/data/user/{username}/avatar(username=*{creatorUsername})}"
-                            alt="example"
-                            onError="this.onerror = null;this.src = '/res/img/avatar-user.png'"
-                            style="width: 30px;height: 30px">
-                    </div>
-                    <div className="col-lg" style="width: 150px">
-                        <a href="" style="color: #775322;font-size: 13px"><b
-                            th:text="*{creatorUsername}">Creator
-                            Username...</b> </a>
-                        <small th:text="*{content}">Content...</small>
-                    </div>
-                </div>
+                                    <div className="row" style={{'marginTop': '-15px'}}>
+                                        <div className="col-md-1" style={{'marginTop': '9px'}}>
+                                            <img
+                                                th:src="@{http://localhost:8001/data/user/{username}/avatar(username=*{creatorUsername})}"
+                                                alt="example"
+                                                onError="this.onerror = null;this.src = '/res/img/avatar-user.png'"
+                                                style="width: 30px;height: 30px">
+                                        </div>
+                                        <div className="col-lg" style="width: 150px">
+                                            <a href="" style="color: #775322;font-size: 13px"><b
+                                                th:text="*{creatorUsername}">Creator
+                                                Username...</b> </a>
+                                            <small th:text="*{content}">Content...</small>
+                                        </div>
+                                    </div>
 
-                {
-                    (() => {
-                        if (isAuthenticated) {
-                            return (
-                                <div className="row">
-                                    <PanePostActions/>
+                                    {
+                                        (() => {
+                                            if (isAuthenticated) {
+                                                return (
+                                                    <div className="row">
+                                                        <PanePostActions/>
+                                                    </div>
+                                                );
+                                            }
+                                        })()
+                                    }
+
                                 </div>
-                            );
-                        }
-                    })()
-                }
-
-            </div>
-        </div>
-
-    );
+                            </div>
+                        );
+                    })
+                })()
+            }
+            </Fragment>
+        );
     }
-    }
+}
 
-    export default PanePost;
+export default PanePost;
