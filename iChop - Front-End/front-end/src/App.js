4991c521c231c435b4e721991958d681bfe86a2b
itsgosho <itsgosho2@gmail.com>
2019-10-06 00:46:35 +0300

    added the Thread create component

--- iChop - Front-End/front-end/src/App.js
+++ iChop - Front-End/front-end/src/App.js
@@ -1,8 +1,6 @@
 import React, {Fragment} from 'react';
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
-import UserOptions from "./components/user/options/UserOptions";
-import UserProfile from "./components/user/profile/UserProfile";
 import TextEditor from "./components/editor/TextEditor";
 
 function App() {
---
+++ iChop - Front-End/front-end/src/components/thread/ThreadCreate.js
@@ -0,0 +1,95 @@
+import React,{Component} from 'react';
+
+class ThreadCreate extends Component {
+
+    render() {
+
+        return (
+            <div id="createThread-page" className="container d-flex justify-content-center align-items-center">
+                <div className="row">
+
+                    <div id="news" className="col-xs-6">
+                        <div className="card">
+                            <div className="card-header">
+                                <small>🆕</small>
+                                Create a new thread:
+                            </div>
+                            <div className="card-body">
+                                <form method="post">
+                                    <div className="row">
+                                        <div className="col-md-12">
+                                            <div className="input-group mb-3">
+                                                <input id="input-title-threadCreate" type="text"
+                                                       className="form-control" aria-label="Title"
+                                                       aria-describedby="basic-addon1" name="title"
+                                                       placeholder="Thread title">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="dropdown-divider"></div>
+
+
+                                    <th:block th:insert="editor/text-editor"></th:block>
+
+                                    <div className="dropdown-divider"></div>
+                                    <div className="col-md-13">
+
+                                        <a href="" type="button" data-toggle="modal" data-target="#exampleModal">
+                                            ❓
+                                        </a>
+                                        <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog"
+                                             aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                            <div className="modal-dialog" role="document">
+                                                <div className="modal-content">
+                                                    <div className="modal-header">
+                                                        <h5 className="modal-title" id="exampleModalLabel">How to show
+                                                            only first X rows from thread on the news page:</h5>
+                                                        <button type="button" className="close" data-dismiss="modal"
+                                                                aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <div className="modal-body">
+                                                        If you dont want all of the content to be show on the news page
+                                                        /better/ ,place SHOW_TO_NOW whether you want to end.
+                                                    </div>
+                                                    <div className="modal-footer">
+                                                        <button type="button" className="btn btn-secondary"
+                                                                data-dismiss="modal">Close
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div id="textarea-content-createThread" contentEditable="true">
+
+                                        </div>
+                                    </div>
+
+                                    <div className="dropdown-divider"></div>
+
+                                    <div className="row">
+                                        <button id="button-proceedCreateThread-createThread" type="button"
+                                                className="btn btn-success btn-lg btn-block">Create
+                                        </button>
+                                    </div>
+
+                                    <script src="/res/js/thread/create-thread-send.js"></script>
+
+                                    <script src="/res/js/editor/text-editor.js"></script>
+
+                                </form>
+                            </div>
+
+                            <div className="dropdown-divider"></div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ThreadCreate;
