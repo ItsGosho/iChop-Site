c56d700349867de2e3bc173a0520b2565d3670e2
itsgosho <itsgosho2@gmail.com>
2019-10-06 01:11:22 +0300

    Moved the text editor`s insert image modal into separate component

--- iChop - Front-End/front-end/src/components/editor/TextEditor.js
+++ iChop - Front-End/front-end/src/components/editor/TextEditor.js
@@ -4,6 +4,7 @@ import TextEditorFontSizePicker from "./TextEditorFontSizePicker";
 import TextEditorFontPicker from "./TextEditorFontPicker";
 import TextEditorAlignPicker from "./TextEditorAlignPicker";
 import TextEditorInsertLinkModal from "./TextEditorInsertLinkModal";
+import TextEditorInsertImageModal from "./TextEditorInsertImageModal";
 
 class TextEditor extends Component {
 
@@ -37,34 +38,7 @@ class TextEditor extends Component {
 
                 <TextEditorInsertLinkModal/>
 
-                <a href="#" id="button-insertImage-textEditor" data-toggle="modal"
-                   data-target="#modal-insertImage-textEditor"
-                   title="Insert Image" onClick={this.preventDefault}><i className="material-icons">photo</i></a>
-                <div className="modal fade" id="modal-insertImage-textEditor" tabIndex="-1" role="dialog"
-                     aria-labelledby="modalLabelInsertImage" aria-hidden="true">
-                    <div className="modal-dialog" role="document">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="modalLabelInsertImage">Image URL</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                <div className="input-group mb-3">
-                                    <input id="input-insertImage-textEditor" type="text" className="form-control"
-                                           aria-describedby="basic-addon1" placeholder=""/>
-                                </div>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                                <button id="button-proceedInsertImage-textEditor" type="button"
-                                        className="btn btn-primary">Insert
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <TextEditorInsertImageModal/>
 
 
                 <a href="#" id="button-undo-textEditor" title="Undo" onClick={this.preventDefault}><i
