e0c4fce9d65cc3824e9a91bc27ca768ef0ea586f
itsgosho <itsgosho2@gmail.com>
2019-10-06 01:09:10 +0300

    Moved the text editor align picker into separate component

--- iChop - Front-End/front-end/src/components/editor/TextEditor.js
+++ iChop - Front-End/front-end/src/components/editor/TextEditor.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import TextEditorColorPicker from "./TextEditorColorPicker";
 import TextEditorFontSizePicker from "./TextEditorFontSizePicker";
 import TextEditorFontPicker from "./TextEditorFontPicker";
+import TextEditorAlignPicker from "./TextEditorAlignPicker";
 
 class TextEditor extends Component {
 
@@ -102,20 +103,7 @@ class TextEditor extends Component {
                    onClick={this.preventDefault}><i
                     className="material-icons">format_clear</i></a>
 
-                <span>
-                                        <a href="#" id="button-textAlign-textEditor" className="dropdown-toggle"
-                                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                                           title="Text Align" onClick={this.preventDefault}><i
-                                            className="material-icons">format_align_justify</i></a>
-                                    <ul className="dropdown-menu scrollable-menu" role="menu">
-                                        <a id="button-alignLeft-textEditor" href="#" onClick={this.preventDefault}><li><i
-                                            className="material-icons">format_align_left</i>Align Left</li></a>
-                                        <a id="button-alignCenter-textEditor" href="#" onClick={this.preventDefault}><li><i
-                                            className="material-icons">format_align_center</i>Align Center</li></a>
-                                        <a id="button-alignRight-textEditor" href="#" onClick={this.preventDefault}><li><i
-                                            className="material-icons">format_align_right</i>Align Right</li></a>
-                                    </ul>
-                                </span>
+                <TextEditorAlignPicker/>
 
 
             </div>
