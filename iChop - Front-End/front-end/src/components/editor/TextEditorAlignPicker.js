0616b1bc3897c2a1a81e080dddef17eb42e0b014
itsgosho <itsgosho2@gmail.com>
2019-10-06 19:52:14 +0300

    Aligh Picker functionality done

--- iChop - Front-End/front-end/src/components/editor/TextEditorAlignPicker.js
+++ iChop - Front-End/front-end/src/components/editor/TextEditorAlignPicker.js
@@ -4,27 +4,27 @@ class TextEditorAlignPicker extends Component {
 
 
     render() {
-        let {execCommand} = this.props;
+        let {execCommand,preventDefault} = this.props;
 
         return (
             <span>
                 <a href="#" id="button-textAlign-textEditor" className="dropdown-toggle"
                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                   title="Text Align" onClick={this.preventDefault}>
+                   title="Text Align" onClick={preventDefault}>
                     <i className="material-icons">format_align_justify</i>
                 </a>
 
                 <ul className="dropdown-menu scrollable-menu" role="menu">
 
-                    <a id="button-alignLeft-textEditor" href="#" onClick={this.preventDefault}>
+                    <a id="button-alignLeft-textEditor" href="#" onClick={execCommand('justifyLeft')}>
                         <li><i className="material-icons">format_align_left</i>Align Left</li>
                     </a>
 
-                    <a id="button-alignCenter-textEditor" href="#" onClick={this.preventDefault}>
+                    <a id="button-alignCenter-textEditor" href="#" onClick={execCommand('justifyCenter')}>
                         <li><i className="material-icons">format_align_center</i>Align Center</li>
                     </a>
 
-                    <a id="button-alignRight-textEditor" href="#" onClick={this.preventDefault}>
+                    <a id="button-alignRight-textEditor" href="#" onClick={execCommand('justifyRight')}>
                         <li><i className="material-icons">format_align_right</i>Align Right</li>
                     </a>
 
