449da32a99637c8b723f46561155647dffefbcfa
itsgosho <itsgosho2@gmail.com>
2019-10-06 20:41:53 +0300

    Finished the Font Size Picker component ,now he is fully functional

--- iChop - Front-End/front-end/src/components/editor/TextEditorFontSizePicker.js
+++ iChop - Front-End/front-end/src/components/editor/TextEditorFontSizePicker.js
@@ -1,48 +1,62 @@
 import React, {Component} from 'react';
+import TextEditorCommands from "./text.editor.commands.constants";
+import CommandExecutorHoc from "./command.executor.hoc";
 
 class TextEditorFontSizePicker extends Component {
 
+    constructor(props) {
+        super(props);
 
+        this.autoFontSizeProceed = this.autoFontSizeProceed.bind(this);
+    }
+
+
+    autoFontSizeProceed(event) {
+        let element = event.target;
+        let size = element.getAttribute('data-content');
+        document.execCommand(TextEditorCommands.SET_FONT_SIZE, false, Number(size));
+    }
 
     render() {
+        let {preventDefault} = this.props;
 
         return (
             <span>
                 <a href="#" id="button-fontSize-textEditor" className="dropdown-toggle"
                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                   title="Font Size" onClick={this.preventDefault}>
+                   title="Font Size" onClick={preventDefault}>
                     <i className="material-icons">format_size</i>
                 </a>
 
                 <ul className="dropdown-menu scrollable-menu" role="menu"
                     style={{'minHeight': '100px', 'maxHeight': '100px', 'overflow': 'auto'}}>
 
-                    <a id="button-fontSize_1-textEditor" data-content="1" href="#" onClick={this.preventDefault}>
-                        <li style={{'fontSize': '9px'}}>1</li>
+                    <a id="button-fontSize_1-textEditor" href="#" onClick={this.autoFontSizeProceed}>
+                        <li data-content="1" style={{'fontSize': '9px'}}>1</li>
                     </a>
 
-                    <a id="button-fontSize_2-textEditor" data-content="2" href="#" onClick={this.preventDefault}>
-                        <li style={{'fontSize': '10px'}}>2</li>
+                    <a id="button-fontSize_2-textEditor" href="#" onClick={this.autoFontSizeProceed}>
+                        <li data-content="2" style={{'fontSize': '10px'}}>2</li>
                     </a>
 
-                    <a id="button-fontSize_3-textEditor" data-content="3" href="#" onClick={this.preventDefault}>
-                        <li style={{'fontSize': '12px'}}>3</li>
+                    <a id="button-fontSize_3-textEditor" href="#" onClick={this.autoFontSizeProceed}>
+                        <li data-content="3" style={{'fontSize': '12px'}}>3</li>
                     </a>
 
-                    <a id="button-fontSize_4-textEditor" data-content="4" href="#" onClick={this.preventDefault}>
-                        <li style={{'fontSize': '15px'}}>4</li>
+                    <a id="button-fontSize_4-textEditor" href="#" onClick={this.autoFontSizeProceed}>
+                        <li data-content="4" style={{'fontSize': '15px'}}>4</li>
                     </a>
 
-                    <a id="button-fontSize_5-textEditor" data-content="5" href="#" onClick={this.preventDefault}>
-                        <li style={{'fontSize': '18px'}}>5</li>
+                    <a id="button-fontSize_5-textEditor" href="#" onClick={this.autoFontSizeProceed}>
+                        <li data-content="5" style={{'fontSize': '18px'}}>5</li>
                     </a>
 
-                    <a id="button-fontSize_6-textEditor" data-content="6" href="#" onClick={this.preventDefault}>
-                        <li style={{'fontSize': '22px'}}>6</li>
+                    <a id="button-fontSize_6-textEditor" href="#" onClick={this.autoFontSizeProceed}>
+                        <li data-content="6" style={{'fontSize': '22px'}}>6</li>
                     </a>
 
-                    <a id="button-fontSize_7-textEditor" data-content="7" href="#" onClick={this.preventDefault}>
-                        <li style={{'fontSize': '26px'}}>7</li>
+                    <a id="button-fontSize_7-textEditor" href="#" onClick={this.autoFontSizeProceed}>
+                        <li data-content="7" style={{'fontSize': '26px'}}>7</li>
                     </a>
 
                 </ul>
@@ -51,4 +65,4 @@ class TextEditorFontSizePicker extends Component {
     }
 }
 
-export default TextEditorFontSizePicker;
+export default CommandExecutorHoc(TextEditorFontSizePicker);
