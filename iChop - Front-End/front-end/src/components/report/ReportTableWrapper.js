f0f776209c0b88ca150ff32cd185e70afd6dc89a
itsgosho <itsgosho2@gmail.com>
2019-10-07 23:22:08 +0300

    Added a props required param - thFirstName ,which will be used for the first TH ,because all other are redudant ,and remove some of the content ,which will be specific for the component that use our wrapper ,left the action button

--- iChop - Front-End/front-end/src/components/report/ReportTableWrapper.js
+++ iChop - Front-End/front-end/src/components/report/ReportTableWrapper.js
@@ -5,12 +5,14 @@ import RoutingURLs from "../../../constants/routing.constants";
 
 var ReportTableWrapper = CreateReactClass({
     render() {
+        let thFirstName = this.props.thFirstName;
+
         return (
-            <div class="table-responsive" align="center">
-                <table class="table" style="width: 85%;text-align: center">
+            <div className="table-responsive" align="center">
+                <table className="table" style={{'width': '85%', 'textAlign': 'center'}}>
                     <thead>
                     <tr>
-                        <th scope="col">Comment Content</th>
+                        <th scope="col">{thFirstName}</th>
                         <th scope="col">Reason</th>
                         <th scope="col">Reporter</th>
                         <th scope="col">Report Date</th>
@@ -19,45 +21,39 @@ var ReportTableWrapper = CreateReactClass({
                     </thead>
                     <tbody>
 
-                    <th:block th:each="report : ${reports}" th:object="${report}">
+                    <tr>
 
-                        <tr class="user-tr">
-                            <td width="300px">
-                                <div style="overflow: scroll;width: 100%;max-height: 100px" th:text="*{commentContent}"></div>
-                            </td>
-                            <td width="300px">
-                                <div style="overflow: scroll;width: 100%;max-height: 100px" th:text="*{reason}"></div>
-                            </td>
-                            <td><a th:href="@{/user/{username}/profile(username=*{creatorUsername})}">ItsGosho</a></td>
-                            <td th:text="*{#temporals.format(reportDate, 'dd MMM,yyyy')}">12 April,2019</td>
-                            <td>
-                                <div class="row">
-                                    <div class="dropdown">
-                                        <button class="btn btn-warning btn-sm dropdown-toggle" type="button" id="dropdownMenuButton"
-                                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            ⚙Take Action!
-                                        </button>
-                                        <div class="dropdown-menu">
-                                            <form method="post" th:action="@{/comment/{commentId}/delete(commentId=*{commentId})}"
-                                                  style="all:initial">
-                                                <a class="dropdown-item" href="#">
-                                                    <button type="submit" style="all: initial">❌Delete Comment</button>
-                                                </a>
-                                            </form>
-                                            <form method="post"
-                                                  th:action="@{/comment/report/{reportId}/delete(reportId=*{reportId})}"
-                                                  style="all:initial">
-                                                <a class="dropdown-item" href="#">
-                                                    <button type="submit" style="all: initial">😖Delete Report</button>
-                                                </a>
-                                            </form>
-                                        </div>
+                        {this.props.children}
+
+                        <td>
+                            <div className="row">
+                                <div className="dropdown">
+                                    <button className="btn btn-warning btn-sm dropdown-toggle" type="button"
+                                            id="dropdownMenuButton"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        ⚙Take Action!
+                                    </button>
+                                    <div className="dropdown-menu">
+                                        <form method="post"
+                                              th:action="@{/comment/{commentId}/delete(commentId=*{commentId})}"
+                                              style="all:initial">
+                                            <a className="dropdown-item" href="#">
+                                                <button type="submit" style="all: initial">❌Delete Comment</button>
+                                            </a>
+                                        </form>
+                                        <form method="post"
+                                              th:action="@{/comment/report/{reportId}/delete(reportId=*{reportId})}"
+                                              style="all:initial">
+                                            <a className="dropdown-item" href="#">
+                                                <button type="submit" style="all: initial">😖Delete Report</button>
+                                            </a>
+                                        </form>
                                     </div>
                                 </div>
-                            </td>
-                        </tr>
+                            </div>
+                        </td>
+                    </tr>
 
-                    </th:block>
                     </tbody>
                 </table>
             </div>
