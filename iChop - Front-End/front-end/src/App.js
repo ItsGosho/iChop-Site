7a8855c8bb76e525be3bc10f00472154570b78bb
Georgi Peev <itsgosho2@gmail.com>
2019-10-07 19:53:27 +0300

    Add the html to the PlayerProfile component

--- iChop - Front-End/front-end/src/App.js
+++ iChop - Front-End/front-end/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar";
 import TextEditor from "./components/editor/TextEditor";
 import ThreadCreate from "./components/thread/ThreadCreate";
 import PlayerLinkAccount from "./components/player/PlayerLinkAccount";
+import PlayerProfile from "./components/player/PlayerProfile";
 
 function App() {
     return (
@@ -11,7 +12,7 @@ function App() {
             <Navbar/>
 
             <div style={{'marginTop':'75px'}}>
-                <PlayerLinkAccount/>
+                <PlayerProfile/>
             </div>
 
             <Footer/>
---
+++ iChop - Front-End/front-end/src/components/player/PlayerProfile.js
@@ -0,0 +1,132 @@
+import React,{Component} from 'react';
+import './PlayerProfile.css'
+
+class PlayerProfile extends Component {
+
+
+    render() {
+
+        return (
+            <div className="container">
+
+                <div className="row">
+
+                    <div className="col-md-auto">
+                        <div className="col-md-sm">
+
+                            <div align="center">
+                                <img th:src="@{https://crafatar.com/renders/body/{uuid}(uuid=*{uuid})}"
+                                     className="img-player_body-player_profile"/>
+                            </div>
+
+
+                            <div className="card menu">
+                                <div className="card-body menu">
+
+                                    <div className="col-md-auto">
+                                        <button className="btn btn-warning btn-statistics" type="button">Statistics
+                                        </button>
+                                    </div>
+
+                                    <div className="col-md-auto">
+                                        <div className="row">
+                                            <!--reserved-->
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <th:block th:if="*{siteUserUsername != null}">
+                                <div className="card site-user-username">
+                                    <div className="card-body site-user-username">
+
+                                        <div className="col-md-auto">
+                                            <div className="row site-user-title">
+                                                <div className="col-md-auto site-user-title">
+                                                    Site User:
+                                                </div>
+                                                <div className="col-md-auto site-user-username">
+                               <span className="site-user-username">
+                                   <a th:href="@{/user/{username}/profile(username=*{siteUserUsername})}"
+                                      th:text="*{siteUserUsername}"></a>
+                               </span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </th:block>
+
+                        </div>
+                    </div>
+
+                    <div className="central-content">
+
+                        <div className="col-md-auto">
+                            <div className="player-name-div">
+                                <div className="row">
+                                    <div>
+                                        <div className="col-md-auto head">
+                                            <span className="player-name" th:text="*{name}"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="dropdown-divider"></div>
+                        </div>
+
+                        <div className="col-md-auto status">
+                            <div className="player-connection-status">
+                                <div className="row">
+                                    <div>
+                                        <div className="col-md-auto head player-connection-status">
+
+                                            <th:block th:if="*{getIsOnline()}">
+                                                <span>🔋<span style="color: green">Online</span></span>
+                                            </th:block>
+
+                                            <th:block th:if="*{!getIsOnline()}">
+                                                <span>🔴<span style="color: darkred">Offline</span></span>
+                                            </th:block>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-auto">
+                            <div className="dropdown-divider"></div>
+                            <div className="col-md-auto head" style="font-size: 19px;font-family: Consolas">
+                                <span>Basic Statistcs:</span>
+                                <div className="row">
+                                    <span>💀</span>Deaths: <span th:text="*{totalDeaths}"></span>
+                                </div>
+                                <div className="row">
+                                    <span>🗡️ </span>Damage Taken: <span th:text="*{totalDamageDealt}"></span>
+                                </div>
+                                <div className="row">
+                                    <span>🤕</span>Damage Taken: <span th:text="*{totalDamageTaken}"></span>
+                                </div>
+                                <div className="row">
+                                    <span>️🧟</span>Mobs Killed: <span th:text="*{totalMobKills}"></span>
+                                </div>
+                                <div className="row">
+                                    <span>👤</span>Player Killed: <span th:text="*{totalPlayerKills}"></span>
+                                </div>
+                                <div className="row">
+                                    <span>⏰</span>Last Join: <span
+                                    th:text="*{#temporals.format(lastJoin, 'dd/MM/yyyy HH:mm')}"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default PlayerProfile;
