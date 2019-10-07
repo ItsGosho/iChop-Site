9bee030c2d292c12f0e76257ec43f670d62cc595
Georgi Peev <itsgosho2@gmail.com>
2019-10-07 20:22:42 +0300

    Refactored the Reports Component to be JSX complient

--- iChop - Front-End/front-end/src/components/report/Reports.js
+++ iChop - Front-End/front-end/src/components/report/Reports.js
@@ -1,4 +1,6 @@
 import React, {Component, Fragment} from 'react';
+import {Link} from "react-router-dom";
+import RoutingURLs from "../../constants/routing.constants";
 
 class Reports extends Component {
 
@@ -8,32 +10,34 @@ class Reports extends Component {
         return (
             <Fragment>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light"
-                     style="margin-left: 40px;margin-right: 40px">
-                    <a className="navbar-brand" href="#">⚠Reports</a>
+                     style={{'marginLeft': '40px', 'marginRight: 40px'}}>
+
+                    <a className="navbar-brand" href="#">⚠ Reports</a>
+
                     <button className="navbar-toggler" type="button" data-toggle="collapse"
                             data-target="#navbarSupportedContent"
                             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon"/>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active">
-                                <a className="nav-link" href="/thread/reports/all">Threads<span
-                                    className="sr-only">(current)</span></a>
+                                <Link className="nav-link" to={RoutingURLs.THREAD.REPORT.ALL}>Threads<span
+                                    className="sr-only">(current)</span></Link>
                             </li>
                             <li className="nav-item active">
-                                <a className="nav-link" href="/comment/reports/all">Comments<span
-                                    className="sr-only">(current)</span></a>
+                                <Link className="nav-link" to={RoutingURLs.COMMENT.REPORT.ALL}>Comments<span
+                                    className="sr-only">(current)</span></Link>
                             </li>
                             <li className="nav-item active">
-                                <a className="nav-link" href="/post/reports/all">Posts<span
-                                    className="sr-only">(current)</span></a>
+                                <Link className="nav-link" to={RoutingURLs.POST.REPORT.ALL}>Posts<span
+                                    className="sr-only">(current)</span></Link>
                             </li>
                         </ul>
                         <form className="form-inline my-2 my-lg-0">
                             <input className="form-control mr-sm-2" id="input-searchByUsername-usersAll" type="search"
-                                   placeholder="Search by username" aria-label="Search">
+                                   placeholder="Search by username" aria-label="Search"/>
                         </form>
                     </div>
                 </nav>
