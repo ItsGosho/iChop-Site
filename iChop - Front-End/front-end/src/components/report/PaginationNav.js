a87c13ca91dd26650a8c91840f7b2a14df5ccb6d
Georgi Peev <itsgosho2@gmail.com>
2019-10-08 20:46:40 +0300

    Added check if pages are divided without reminder

--- iChop - Front-End/front-end/src/components/report/PaginationNav.js
+++ iChop - Front-End/front-end/src/components/report/PaginationNav.js
@@ -5,22 +5,15 @@ import Pagination from "react-bootstrap/Pagination";
 
 class PaginationNav extends Component {
 
-    constructor(props) {
-        super(props);
-
-        this.onChange = this.onChange.bind(this);
-    }
-
-    onChange() {
-        console.log(123);
-    }
-
 
     render() {
         // eslint-disable-next-line no-restricted-globals
         let page = qs.parse(location.search, {ignoreQueryPrefix: true}).page;
 
-        let totalPages = 2;
+        let totalResults = 50;
+        let resultsPerPage = 20;
+
+        let totalPages = totalResults / resultsPerPage + (totalResults % resultsPerPage !== 0 ? 1 : 0);
         let redirectPage = '/thread/reports/all';
 
         page = page === undefined ? 1 : Number(page);
