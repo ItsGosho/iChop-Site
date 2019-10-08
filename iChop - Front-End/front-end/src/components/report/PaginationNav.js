8260b682f4c9f0988ec8cf9d32f72d8757acc20c
itsgosho <itsgosho2@gmail.com>
2019-10-09 00:10:54 +0300

    Added my own formula to calculate how much page blocks to show in the navigation

--- iChop - Front-End/front-end/src/components/report/PaginationNav.js
+++ iChop - Front-End/front-end/src/components/report/PaginationNav.js
@@ -5,18 +5,36 @@ import Pagination from "react-bootstrap/Pagination";
 
 class PaginationNav extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            totalResults: 35,
+            resultsPerPage: 1,
+            minPageButtons: 1,
+            maxPageButtons: 10,
+        }
+    }
+
 
     render() {
         // eslint-disable-next-line no-restricted-globals
         let page = qs.parse(location.search, {ignoreQueryPrefix: true}).page;
 
-        let totalResults = 15;
+        let totalResults = 35;
         let resultsPerPage = 1;
 
         let totalPages = totalResults / resultsPerPage + (totalResults % resultsPerPage !== 0 ? 1 : 0);
         let redirectPage = '/thread/reports/all';
 
         page = page === undefined ? 1 : Number(page);
+
+        let minPageButtons = 1;
+        let maxPageButtons = 10;
+
+        minPageButtons = Math.floor(page / maxPageButtons) * maxPageButtons;
+        maxPageButtons = (minPageButtons + maxPageButtons) > totalResults ? totalResults : (minPageButtons + maxPageButtons);
+
 
         return (
             <Pagination className={'justify-content-center'}>
@@ -37,7 +55,7 @@ class PaginationNav extends Component {
                 {
                     (() => {
                             let result = [];
-                            for (let i = 1; i <= totalResults; i++) {
+                            for (let i = minPageButtons; i <= maxPageButtons; i++) {
                                 result.push(
                                     <Pagination.Item>
                                         <Link to={redirectPage + '?page=' + i}>{i}</Link>
