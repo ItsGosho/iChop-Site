0a5f0d64aa3978bf6e317fb5662600be5813c7b1
Georgi Peev <itsgosho2@gmail.com>
2019-10-08 20:07:24 +0300

    First part of the PaginationNav is done

--- iChop - Front-End/front-end/src/components/report/PaginationNav.js
+++ iChop - Front-End/front-end/src/components/report/PaginationNav.js
@@ -6,27 +6,38 @@ class PaginationNav extends Component {
 
     render() {
         // eslint-disable-next-line no-restricted-globals
-        let page = qs.parse(location.search,{ignoreQueryPrefix: true}).page;
+        let page = qs.parse(location.search, {ignoreQueryPrefix: true}).page;
+        console.log(page);
 
         return (
             <nav aria-label="Page navigation example" className="d-flex justify-content-center align-items-center">
                 <ul className="pagination">
 
-                    <th:block th:if="${#request.getParameter('page')} != null">
+                    {
+                        (() => {
+                                if (page !== undefined && page - 1 >= 0) {
+                                    return (
+                                        <li className="page-item"><a className="page-link"
+                                                                     th:href="@{/post/reports/all(page=${(#conversions.convert(#request.getParameter('page'), 'Integer')) - 1})}">Previous</a>
+                                        </li>
+                                    );
+                                }
+                            }
+                        )()
+                    }
 
-                        <th:block
-                            th:if="${(#conversions.convert(#request.getParameter('page'), 'Integer')) - 1} >= 0">
-                            <li className="page-item"><a className="page-link"
-                                                         th:href="@{/post/reports/all(page=${(#conversions.convert(#request.getParameter('page'), 'Integer')) - 1})}">Previous</a>
-                            </li>
-                        </th:block>
-
-                    </th:block>
 
                     <th:block th:each="i: *{#numbers.sequence(1,totalPages)}">
                         <li className="page-item"><a className="page-link" th:text="${i}"
                                                      th:href="@{/post/reports/all(page=${i-1})}"></a></li>
                     </th:block>
+
+                    {
+                        (() => {
+                              return ()
+                            }
+                        )()
+                    }
 
 
                     <th:block th:if="${#request.getParameter('page')} != null">
