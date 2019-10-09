81bfc953dca3a95a1e34c6a04583194c6888eb2c
Georgi Peev <itsgosho2@gmail.com>
2019-10-09 20:06:20 +0300

    Removed the isAuthenticated from the Comment button in ThreadReadAddComment component

--- iChop - Front-End/front-end/src/components/thread/read/ThreadReadAddComment.js
+++ iChop - Front-End/front-end/src/components/thread/read/ThreadReadAddComment.js
@@ -19,20 +19,21 @@ class ThreadReadAddComment extends Component {
 
                                     <div id="textarea-content-createComment"
                                          className="thread-comment_box-textarea"
-                                         contentEditable="true">
+                                         contentEditable="true"/>
 
-                                    </div>
                                     <div
                                         className="row d-flex justify-content-center align-items-center thread-comment_box-buttons">
-                                        <button sec:authorize="isAuthenticated()"
-                                                id="button-createCommentThread-readThreadPage"
-                                                className="btn btn-sm" type="button">
+
+                                        <button id="button-createCommentThread-readThreadPage" className="btn btn-sm"
+                                                type="button">
                                             Comment
                                         </button>
+
                                         <button id="button-cancelCommentThread-readThreadPage"
                                                 className="btn btn-sm thread-comment_box-button_cancel" type="button">
                                             Cancel
                                         </button>
+
                                     </div>
                                 </div>
                             );
