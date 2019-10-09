f7965d8b9541d216d9d7d5b78590af082a4ca9d3
itsgosho <itsgosho2@gmail.com>
2019-10-10 00:38:54 +0300

    Minor text bug fixes and changes

--- iChop - Front-End/front-end/src/components/thread/read/ThreadReadMainContent.js
+++ iChop - Front-End/front-end/src/components/thread/read/ThreadReadMainContent.js
@@ -239,58 +239,57 @@ class ThreadReadMainContent extends Component {
                                     }
 
                                 </div>
+                                <div className="btn-group thread-right_side_buttons">
+                                    {
+                                        (() => {
+                                            if (isAuthenticated) {
+                                                return (
+                                                    <Fragment>
+                                                        <button
+                                                            id="button-commentThread-readThreadPage"
+                                                            className="btn btn-sm" type="button"
+                                                            aria-haspopup="true" aria-expanded="false">
+                                                            <small>💬</small>
+                                                            <span>Comment</span>
+                                                        </button>
 
+                                                        <button className="btn btn-sm dropdown-toggle"
+                                                                type="button"
+                                                                data-toggle="dropdown" aria-haspopup="true"
+                                                                aria-expanded="false">
+                                                            <small>💡</small>
+                                                            React
+                                                        </button>
 
-                                {
-                                    (() => {
-                                        if (isAuthenticated) {
-                                            return (
-                                                <div className="btn-group thread-right_side_buttons">
+                                                        {
+                                                            (() => {
+                                                                if (!isLikedThreadAlready) {
+                                                                    return (
+                                                                        <div className="dropdown-menu">
+                                                                            <button
+                                                                                className="btn btn-sm thread-right_side_button-react"
+                                                                                type="button">
+                                                                                <small>👍🏻</small>
+                                                                                <span> Like</span>
+                                                                            </button>
+                                                                            <button
+                                                                                className="btn btn-sm thread-right_side_button-react"
+                                                                                type="button">
+                                                                                <small>👎🏻</small>
+                                                                                <span> Dislike</span>
+                                                                            </button>
+                                                                        </div>
+                                                                    );
+                                                                }
+                                                            })()
+                                                        }
 
-                                                    <button
-                                                        id="button-commentThread-readThreadPage"
-                                                        className="btn btn-sm" type="button"
-                                                        aria-haspopup="true" aria-expanded="false">
-                                                        <small>💬</small>
-                                                        <span>Comment</span>
-                                                    </button>
-
-                                                    <button className="btn btn-sm dropdown-toggle"
-                                                            type="button"
-                                                            data-toggle="dropdown" aria-haspopup="true"
-                                                            aria-expanded="false">
-                                                        <small>💡</small>
-                                                        React
-                                                    </button>
-
-                                                    {
-                                                        (() => {
-                                                            if (!isLikedThreadAlready) {
-                                                                return (
-                                                                    <div className="dropdown-menu">
-                                                                        <button
-                                                                            className="btn btn-sm thread-right_side_button-react"
-                                                                            type="button">
-                                                                            <small>👍🏻</small>
-                                                                            <span> Like</span>
-                                                                        </button>
-                                                                        <button
-                                                                            className="btn btn-sm thread-right_side_button-react"
-                                                                            type="button">
-                                                                            <small>👎🏻</small>
-                                                                            <span> Dislike</span>
-                                                                        </button>
-                                                                    </div>
-                                                                );
-                                                            }
-                                                        })()
-                                                    }
-
-                                                </div>
-                                            );
-                                        }
-                                    })()
-                                }
+                                                    </Fragment>
+                                                );
+                                            }
+                                        })()
+                                    }
+                                </div>
                             </div>
                         </div>
                     </div>
