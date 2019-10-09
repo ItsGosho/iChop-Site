63a0d6d40a7f190d852c965b4b48274dbe7f5384
Georgi Peev <itsgosho2@gmail.com>
2019-10-09 19:47:34 +0300

    Added the delete button into check if the user is authenticated and is moderator

--- iChop - Front-End/front-end/src/components/thread/read/ThreadReadMainContent.js
+++ iChop - Front-End/front-end/src/components/thread/read/ThreadReadMainContent.js
@@ -166,25 +166,22 @@ class ThreadReadMainContent extends Component {
                                         })()
                                     }
 
-                                    <button sec:authorize="isAuthenticated() && hasAuthority('MODERATOR')"
-                                            className="btn btn-secondary btn-sm dropdown-toggle" type="button"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <small>⚙</small>
-                                        Options
-                                    </button>
-
                                     <div className="dropdown-menu">
-                                        <form className="dropdown-item"
-                                              th:action="@{/thread/{id}/delete(id=*{id})}"
-                                              method="post">
-                                            <button
-                                                sec:authorize="isAuthenticated() && hasAuthority('MODERATOR')"
-                                                type="submit"
-                                                className="btn btn-light btn-sm thread-delete_button">
-                                                <small>❌</small>
-                                                Delete
-                                            </button>
-                                        </form>
+                                        {
+                                            (() => {
+                                                if (isAuthenticated && hasRoleModerator) {
+                                                    return (
+                                                        <button
+                                                            type="button"
+                                                            className="btn btn-light btn-sm thread-delete_button">
+                                                            <small>❌</small>
+                                                            <span>Delete</span>
+                                                        </button>
+                                                    );
+                                                }
+                                            })()
+                                        }
+
                                     </div>
 
                                     <button th:id="'thread-report_button-'+*{id}"
