16a2392cbd2d9a060bd752d7759adbc032be89c6
Georgi Peev <itsgosho2@gmail.com>
2019-10-10 14:45:48 +0300

    Moved the TextEditorAlignPicker material icons types into constants

--- iChop - Front-End/front-end/src/components/editor/TextEditorAlignPicker.js
+++ iChop - Front-End/front-end/src/components/editor/TextEditorAlignPicker.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import CommandExecutorHoc from "./command.executor.hoc";
 import TextEditorCommands from "./text.editor.commands.constants";
+import MaterialIcons from "../../constants/material.icons.types.constants";
 
 class TextEditorAlignPicker extends Component {
 
@@ -13,21 +14,21 @@ class TextEditorAlignPicker extends Component {
                 <a href="#"  className="dropdown-toggle"
                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
                    title="Text Align" onClick={preventDefault}>
-                    <i className="material-icons">format_align_justify</i>
+                    <i className="material-icons">{MaterialIcons.FORMAT_ALIGN_JUSTIFY}</i>
                 </a>
 
                 <ul className="dropdown-menu scrollable-menu" role="menu">
 
                     <a href="#" onClick={execCommand(TextEditorCommands.ALIGN_LEFT)}>
-                        <li><i className="material-icons">format_align_left</i>Align Left</li>
+                        <li><i className="material-icons">{MaterialIcons.FORMAT_ALIGN_LEFT}</i>Align Left</li>
                     </a>
 
                     <a href="#" onClick={execCommand(TextEditorCommands.ALIGN_CENTER)}>
-                        <li><i className="material-icons">format_align_center</i>Align Center</li>
+                        <li><i className="material-icons">{MaterialIcons.FORMAT_ALIGN_CENTER}</i>Align Center</li>
                     </a>
 
                     <a href="#" onClick={execCommand(TextEditorCommands.ALIGN_RIGHT)}>
-                        <li><i className="material-icons">format_align_right</i>Align Right</li>
+                        <li><i className="material-icons">{MaterialIcons.FORMAT_ALIGN_RIGHT}</i>Align Right</li>
                     </a>
 
                 </ul>
--- iChop - Front-End/front-end/src/constants/material.icons.types.constants.js
+++ iChop - Front-End/front-end/src/constants/material.icons.types.constants.js
@@ -6,8 +6,14 @@ const MaterialIcons = {
     FORMAT_ITALIC: 'format_italic',
     FORMAT_UNDERLINE: 'format_underlined',
     FORMAT_CLEAR: 'format_clear',
+
     UNDO: 'undo',
     REDO: 'redo',
+
+    FORMAT_ALIGN_JUSTIFY: 'format_align_justify',
+    FORMAT_ALIGN_LEFT: 'format_align_left',
+    FORMAT_ALIGN_RIGHT: 'format_align_right',
+    FORMAT_ALIGN_CENTER: 'format_align_center',
 };
 
 export default MaterialIcons;
