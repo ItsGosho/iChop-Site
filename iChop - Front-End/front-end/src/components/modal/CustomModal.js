43563041fd1bf25c8f336c37b72e3648a0b9b13d
Georgi Peev <itsgosho2@gmail.com>
2019-10-10 12:47:17 +0300

    Added CustomModal component with its initial template

---
+++ iChop - Front-End/front-end/src/components/modal/CustomModal.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class CustomModal extends Component {
+
+
+    render() {
+
+        return (
+            <div className="modal fade" id="modal-insertImage-textEditor" tabIndex="-1" role="dialog"
+                 aria-labelledby="modalLabelInsertImage" aria-hidden="true">
+
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                             
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default CustomModal;
