511b618f3cbdddede54124108e955d8b3b439a9e
Georgi Peev <itsgosho2@gmail.com>
2019-10-10 12:48:59 +0300

    Added id and relationTo (for aria-labelledby) in the CustomModal component

--- iChop - Front-End/front-end/src/components/modal/CustomModal.js
+++ iChop - Front-End/front-end/src/components/modal/CustomModal.js
@@ -4,14 +4,15 @@ class CustomModal extends Component {
 
 
     render() {
+        let {id, relationTo} = this.props;
 
         return (
-            <div className="modal fade" id="modal-insertImage-textEditor" tabIndex="-1" role="dialog"
-                 aria-labelledby="modalLabelInsertImage" aria-hidden="true">
+            <div className="modal fade" id={id} tabIndex="-1" role="dialog"
+                 aria-labelledby={relationTo} aria-hidden="true">
 
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
-                             
+
                     </div>
                 </div>
             </div>
