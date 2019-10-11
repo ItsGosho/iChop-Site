7ba29809b7238dc5da4c1d3b90ada6bf9d43b018
Georgi Peev <itsgosho2@gmail.com>
2019-10-11 18:18:09 +0300

    Added routing in the player profile

--- iChop - Front-End/front-end/src/components/player/grid/PlayerProfileCenter.js
+++ iChop - Front-End/front-end/src/components/player/grid/PlayerProfileCenter.js
@@ -1,15 +1,21 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import PlayerStatistics from "../profile/PlayerStatistics";
+import {Route, Switch} from "react-router-dom";
+import RoutingURLs from "../../../constants/routing.constants";
 
 class PlayerProfileCenter extends Component {
 
 
     render() {
+        let uuid = '8ed20904-3262-401a-901a-1946504d2eea';
 
         return (
             <div className="central-content">
 
-                <PlayerStatistics/>
+                <Switch>
+                    <Route exact path={RoutingURLs.PLAYER.PROFILE.VIEW.replace(':uuid',uuid)} component={() => (<PlayerStatistics/>)}/>
+                    <Route exact path={RoutingURLs.PLAYER.PROFILE.STATISTICS} component={() => (<PlayerStatistics/>)}/>
+                </Switch>
 
             </div>
         );
