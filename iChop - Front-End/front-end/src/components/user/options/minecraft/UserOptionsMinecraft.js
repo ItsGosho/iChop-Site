8a9f656867d8141e50ef4f72eadb496c056976c5
Georgi Peev <itsgosho2@gmail.com>
2019-10-13 14:32:05 +0300

    Inlined the if check in the UserOptionsMinecraft component

--- iChop - Front-End/front-end/src/components/user/options/minecraft/UserOptionsMinecraft.js
+++ iChop - Front-End/front-end/src/components/user/options/minecraft/UserOptionsMinecraft.js
@@ -15,64 +15,55 @@ class UserOptionsMinecraft extends Component {
 
         return (
             <Fragment>
+                {!isAccountLinked ? (
+                    <Fragment>
+                        <div className="dropdown-divider"/>
 
-                {
-                    (() => {
-                        if (!isAccountLinked) {
-                            return (
-                                <Fragment>
-                                    <div className="dropdown-divider"/>
+                        <div className="row" align="center">
+                            <div className="col-lg">
+                                <span>You have not linked account yet!</span>
+                            </div>
+                        </div>
 
-                                    <div className="row" align="center">
-                                        <div className="col-lg">
-                                            <span>You have not linked account yet!</span>
-                                        </div>
-                                    </div>
+                        <div className="dropdown-divider"/>
 
-                                    <div className="dropdown-divider"/>
-
-                                    <div className="row" align="center">
-                                        <div className="col-lg">
+                        <div className="row" align="center">
+                            <div className="col-lg">
                                            <span>To link your account you must be logged in the site with the profile that you want to link your account
                                            ,then in the game type <span><b>/linkaccount</b></span> ,then a <span><b>link</b></span> will be generated ,click
                                                it and a page <span><b>in the site</b></span> will open
                                            with confirmation to link your account.Note that if you link your account you cant link another until <span><b>the
                                            previous is unlinked</b></span>.</span>
-                                        </div>
-                                    </div>
+                            </div>
+                        </div>
 
-                                    <div className="dropdown-divider"/>
-                                </Fragment>
-                            );
-                        } else {
-                            return (
-                                <Fragment>
-                                    <div className="row" align="center">
-                                        <div className="col-lg">
+                        <div className="dropdown-divider"/>
+                    </Fragment>
+                ) : (
+                    <Fragment>
+                        <div className="row" align="center">
+                            <div className="col-lg">
                                             <span>
                                                 <b>
                                                     <Link to={accountProfile}>{playerName}</Link>
                                                 </b>
                                             </span>
-                                        </div>
-                                    </div>
+                            </div>
+                        </div>
 
-                                    <div className="dropdown-divider"/>
+                        <div className="dropdown-divider"/>
 
-                                    <div align="center">
-                                        <img src={crafatarSkinUrl} style={{'width': '95px', 'height': '200px'}} alt=''/>
-                                    </div>
+                        <div align="center">
+                            <img src={crafatarSkinUrl} style={{'width': '95px', 'height': '200px'}} alt=''/>
+                        </div>
 
-                                    <div className="dropdown-divider"/>
+                        <div className="dropdown-divider"/>
 
-                                    <div align="center" style={{'marginTop': '10px'}}>
-                                        <button type="button" className="btn btn-warning">Unlink</button>
-                                    </div>
-                                </Fragment>
-                            );
-                        }
-                    })()
-                }
+                        <div align="center" style={{'marginTop': '10px'}}>
+                            <button type="button" className="btn btn-warning">Unlink</button>
+                        </div>
+                    </Fragment>
+                )}
             </Fragment>
         );
     }
