c106ef024d7026eac4c8db921d158a54da1bf636
Georgi Peev <itsgosho2@gmail.com>
2019-10-13 15:26:35 +0300

    Refactored and moved the password and confirmPassword into separate component.The ChangePassword will now use it

---
+++ iChop - Front-End/front-end/src/components/other/PassworsForm.js
@@ -0,0 +1,109 @@
+import React, {Component, Fragment} from 'react';
+
+class PassworsForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isPasswordsShown: false
+        };
+
+        this.onShowPasswords = this.onShowPasswords.bind(this);
+        this.onChange = this.onChange.bind(this);
+
+        this.passwordRef = React.createRef();
+        this.confirmPasswordRef = React.createRef();
+    }
+
+
+    onShowPasswords() {
+        let passwordElement = this.passwordRef.current;
+        let confirmPasswordElement = this.confirmPasswordRef.current;
+
+        if (this.state.isPasswordsShown) {
+            passwordElement.type = 'password';
+            confirmPasswordElement.type = 'password';
+        } else {
+            passwordElement.type = 'text';
+            confirmPasswordElement.type = 'text';
+        }
+
+        this.setState((prevState) => ({isPasswordsShown: !prevState.isPasswordsShown}))
+    }
+
+    onChange() {
+        let password = this.passwordRef.current.value;
+        let confirmPassword = this.confirmPasswordRef.current.value;
+
+        if (password !== confirmPassword) {
+            this.passwordRef.current.style['border-color'] = 'red';
+            this.confirmPasswordRef.current.style['border-color'] = 'red';
+        } else {
+            this.passwordRef.current.style['border-color'] = '';
+            this.confirmPasswordRef.current.style['border-color'] = '';
+        }
+    }
+
+    render() {
+        let {onChange} = this.props;
+
+        return (
+            <Fragment>
+                <div className="form-group">
+                    <div className="input-group mb-2">
+
+                        <div className="input-group-prepend">
+                            <div className="input-group-text">🔒</div>
+                        </div>
+
+                        <input type="password"
+                               className="form-control"
+                               autoComplete="off"
+                               data-trigger="focus"
+                               name="password"
+                               placeholder="Password..."
+                               ref={this.passwordRef}
+                               onChange={(event) => {
+                                   onChange(event);
+                                   this.onChange(event);
+                               }}/>
+
+                        <div className="input-group-append">
+                            <button type="button"
+                                    className="btn btn-outline-success"
+                                    onClick={this.onShowPasswords}>
+                                Show
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="form-group">
+                    <div className="input-group mb-2">
+
+                        <div className="input-group-prepend">
+                            <div className="input-group-text">🔒</div>
+                        </div>
+
+                        <input type="password"
+                               className="form-control"
+                               autoComplete="off"
+                               name="confirmPassword"
+                               placeholder="Confirm Password..."
+                               ref={this.confirmPasswordRef}
+                               onChange={(event) => {
+                                   onChange(event);
+                                   this.onChange(event);
+                               }}/>
+
+                    </div>
+                </div>
+            </Fragment>
+        );
+    }
+
+}
+
+export default PassworsForm;
--- iChop - Front-End/front-end/src/components/user/options/password/UserOptionsChangePassword.js
+++ iChop - Front-End/front-end/src/components/user/options/password/UserOptionsChangePassword.js
@@ -1,39 +1,22 @@
 import React, {Component} from 'react';
+import PassworsForm from "../../../other/PassworsForm";
 
 class UserOptionsChangePassword extends Component {
 
     constructor(props) {
         super(props);
 
-        this.state = {
-            isPasswordsShown: false
-        };
 
         this.onResetClick = this.onResetClick.bind(this);
-        this.onShowPasswords = this.onShowPasswords.bind(this);
-
-        this.passwordRef = React.createRef();
-        this.confirmPasswordRef = React.createRef();
     }
 
-    onShowPasswords() {
-        let passwordElement = this.passwordRef.current;
-        let confirmPasswordElement = this.confirmPasswordRef.current;
-
-        if (this.state.isPasswordsShown) {
-            passwordElement.type = 'password';
-            confirmPasswordElement.type = 'password';
-        } else {
-            passwordElement.type = 'text';
-            confirmPasswordElement.type = 'text';
-        }
-
-        this.setState((prevState) => ({isPasswordsShown: !prevState.isPasswordsShown}))
-    }
 
     onResetClick() {
-        console.log(this.state.password);
-        console.log(this.state.confirmPassword);
+        /*RESET*/
+        let {password, confirmPassword} = this.state;
+
+        console.log(password);
+        console.log(confirmPassword);
     }
 
     render() {
@@ -42,49 +25,18 @@ class UserOptionsChangePassword extends Component {
         return (
             <form className="px-4 py-3">
 
-                <div className="form-group">
-                    <div className="input-group mb-2">
+                <PassworsForm ref onChange={onChange}/>
 
-                        <div className="input-group-prepend">
-                            <div className="input-group-text">🔒</div>
-                        </div>
+                <div align="center">
 
-                        <input type="password"
-                               className="form-control"
-                               autoComplete="off"
-                               data-trigger="focus"
-                               name="password"
-                               placeholder="Password..."
-                               ref={this.passwordRef}
-                               onChange={onChange}/>
+                    <button type="button"
+                            data-style="zoom-in"
+                            className="btn btn-primary btn-ladda"
+                            onClick={this.onResetClick}>
+                        Reset
+                    </button>
 
-                        <div className="input-group-append">
-                            <button type="button"
-                                    className="btn btn-outline-success"
-                                    onClick={this.onShowPasswords}>
-                                Show
-                            </button>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div className="form-group">
-                    <div className="input-group mb-2">
-
-                        <div className="input-group-prepend">
-                            <div className="input-group-text">🔒</div>
-                        </div>
-
-                        <input type="password"
-                               className="form-control"
-                               autoComplete="off"
-                               name="confirmPassword"
-                               placeholder="Confirm Password..."
-                               ref={this.confirmPasswordRef}
-                               onChange={onChange}/>
-
-                    </div>
+                    <div className="dropdown-divider"/>
                 </div>
 
             </form>
