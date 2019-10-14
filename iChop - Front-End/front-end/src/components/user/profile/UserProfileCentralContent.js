a7e6b269e81ceaa193396cd4aaf0c4b51cfa996b
Georgi Peev <itsgosho2@gmail.com>
2019-10-14 20:16:10 +0300

    Moved the user profile central top information into separate component

--- iChop - Front-End/front-end/src/components/user/profile/UserProfileCentralContent.js
+++ iChop - Front-End/front-end/src/components/user/profile/UserProfileCentralContent.js
@@ -1,81 +1,16 @@
 import React, {Component, Fragment} from 'react';
 import './UserProfileCentralContent.css'
-import Roles from "../../../constants/roles.constants";
+import UserProfileCentralHead from "./UserProfileCentralHead";
 
 class UserProfileCentralContent extends Component {
 
 
     render() {
-        let username = 'ItsGosho';
-        let role = Roles.ADMIN;
-        let statusMessage = 'Hello!';
-        let isAuthenticated = true;
 
         return (
             <Fragment>
-                <div className="central-content">
 
-                    <div className="col-md-auto username_and_rank_follow_unfollow">
-                        <div className="div-first-head">
-                            <div className="row">
-                                <div>
-                                    <div className="col-md-auto head">
-                                        <span className="div-first-content">{username}</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div>
-                                    <div className="col-md-auto head">
-                                        <span className="div-first-content">{role}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        {isAuthenticated ? (
-                            <Fragment>
-                                <div className="row">
-                                    <div className="col-md-12 head">
-                                        <a href=' '
-                                           onClick={(event) => {
-                                               event.preventDefault();
-                                           }}
-                                           className="follow-controls">Follow</a>
-                                        <a href=' ' onClick={(event) => {
-                                            event.preventDefault();
-                                        }}
-                                           className="follow-controls">Unfollow</a>
-
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-md-12 head">
-                                        <small className="is-followed-you"/>
-                                    </div>
-                                </div>
-                            </Fragment>
-                        ) : null}
-
-                        <div className="dropdown-divider"/>
-                    </div>
-
-                    <div className="col-md-auto status">
-                        <div className="status-div">
-                            <div className="row">
-                                <div>
-
-                                    {statusMessage != null ? (
-                                        <div className="col-md-auto head">
-                                            <span className="status-message">{statusMessage}</span>
-                                        </div>
-                                    ) : null}
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <UserProfileCentralHead/>
 
                 <div className="col-md-auto user-information-navigation">
 
