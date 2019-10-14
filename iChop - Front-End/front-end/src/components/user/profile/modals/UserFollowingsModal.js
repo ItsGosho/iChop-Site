0bc3ba56dbb2020a981d7a37bea3af9a8a5506e3
itsgosho <itsgosho2@gmail.com>
2019-10-14 23:20:23 +0300

    Added mock data for the FollowingsModal and moved the iteration in method + the unfollow is moved into method

--- iChop - Front-End/front-end/src/components/user/profile/modals/UserFollowingsModal.js
+++ iChop - Front-End/front-end/src/components/user/profile/modals/UserFollowingsModal.js
@@ -1,8 +1,63 @@
 import React, {Component} from 'react';
 import Image from "../../../other/Image";
 import FrontEndResourcesRoutingURLs from "../../../../constants/front-end.resources.routings";
+import RoutingURLs from "../../../../constants/routing.constants";
+import {Link} from "react-router-dom";
 
 class UserFollowingsModal extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            followings: [
+                {username: 'Ivan'},
+                {username: 'Qncho'},
+            ]
+        };
+
+        this.unFollow = this.unFollow.bind(this);
+        this.iterFollowings = this.iterFollowings.bind(this);
+    }
+
+    unFollow(username) {
+        console.log(`Unfollow: ${username}`);
+    }
+
+
+    iterFollowings() {
+        return this.state.followings.map((following, index) => {
+            let {username} = following;
+            let profileUrl = RoutingURLs.USER.PROFILE.VIEW.replace(':username', username);
+
+            return (
+                <div className="w-100" style={{'marginTop': '10px'}}>
+
+                    <Image url={'/'}
+                           defaultUrl={FrontEndResourcesRoutingURLs.USER.AVATAR}
+                           style={{'width': '50px', 'height': '50px', 'marginLeft': '15px'}}/>
+
+                    <span style={{'marginLeft': '15px'}}>
+                        <b>
+                            <Link to={profileUrl}>{username}</Link>
+                        </b>
+                    </span>
+
+                    <button className="btn btn-warning btn-sm float-right"
+                            style={{
+                                'fontFamily': 'Consolas',
+                                'marginTop': '15px',
+                                'marginRight': '15px'
+                            }}
+                            onClick={() => {
+                                this.unFollow(username)
+                            }}>
+                        Unfollow
+                    </button>
+                </div>
+            )
+        })
+    }
 
     render() {
 
@@ -22,36 +77,9 @@ class UserFollowingsModal extends Component {
                              style={{'overflow': 'auto', 'maxHeight': '400px'}}>
 
                             <div className="row">
-                                <div className="w-100" style={{'marginTop':'10px'}}>
 
-                                    <Image url={'/'}
-                                           defaultUrl={FrontEndResourcesRoutingURLs.USER.AVATAR}
-                                           style={{'width': '50px', 'height': '50px', 'marginLeft': '15px'}}/>
+                                {this.iterFollowings()}
 
-                                    <span style={{'marginLeft': '15px'}}><b><a href="{profileURL}">Joreto</a></b></span>
-                                    <button className="btn btn-warning btn-sm float-right" style={{
-                                        'fontFamily': 'Consolas',
-                                        'marginTop': '15px',
-                                        'marginRight': '15px'
-                                    }}>
-                                        Unfollow
-                                    </button>
-                                </div>
-                                <div className="w-100" style={{'marginTop':'10px'}}>
-
-                                    <Image url={'/'}
-                                           defaultUrl={FrontEndResourcesRoutingURLs.USER.AVATAR}
-                                           style={{'width': '50px', 'height': '50px', 'marginLeft': '15px'}}/>
-
-                                    <span style={{'marginLeft': '15px'}}><b><a href="{profileURL}">Joreto</a></b></span>
-                                    <button className="btn btn-warning btn-sm float-right" style={{
-                                        'fontFamily': 'Consolas',
-                                        'marginTop': '15px',
-                                        'marginRight': '15px'
-                                    }}>
-                                        Unfollow
-                                    </button>
-                                </div>
                             </div>
 
                         </div>
