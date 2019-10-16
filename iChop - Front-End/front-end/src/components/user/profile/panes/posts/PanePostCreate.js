5a04e3b8b8aaebb14a97885c3346d994ea9ffae2
itsgosho <itsgosho2@gmail.com>
2019-10-16 09:26:39 +0300

    The TextArea for creating a post in the PanePostCreate is finished ,the counting of characters works too

--- iChop - Front-End/front-end/src/components/user/profile/panes/posts/PanePostCreate.js
+++ iChop - Front-End/front-end/src/components/user/profile/panes/posts/PanePostCreate.js
@@ -3,14 +3,42 @@ import ServerRoutingURLs from "../../../../../constants/server.routing.urls";
 import FrontEndResourcesRoutingURLs from "../../../../../constants/front-end.resources.routings";
 import './PanePostCreate.css';
 import Image from "../../../../other/Image";
+import TextAreaWithCounter from "../../../../other/TextAreaWithCounter";
+import {PostValidationConstants} from "../../../../../constants/validation.constants";
 
 class PanePostCreate extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            content: '',
+
+            leftCharacters: PostValidationConstants.MAX_CHARACTERS
+        };
+
+        this.onCreate = this.onCreate.bind(this);
+        this.onContentCountedCharacters = this.onContentCountedCharacters.bind(this);
+        this.onContentValueChange = this.onContentValueChange.bind(this);
+    }
+
+    onContentCountedCharacters(leftCharacters) {
+        this.setState({leftCharacters: leftCharacters})
+    }
+
+    onContentValueChange(value) {
+        this.setState({content: value})
+    }
+
+    onCreate() {
+        let {content} = this.state;
+        console.log('Create!');
+    }
 
     render() {
         let username = 'ItsGosho';
         let userAvatarUrl = ServerRoutingURLs.DATA.USER.AVATAR.GET.replace(':username', username);
 
-        /*TODO: count left chars like in the profile edit and add method for the post button*/
         return (
             <form>
                 <div className="row">
@@ -22,15 +50,27 @@ class PanePostCreate extends Component {
                     </div>
 
                     <div className="col-lg">
-                        <textarea name="content" className="pane-post-create-post"/>
+
+                        <TextAreaWithCounter name="content"
+                                             value={this.state.content}
+                                             className="pane-post-create-post"
+                                             onCounted={this.onContentCountedCharacters}
+                                             onValueChange={this.onContentValueChange}
+                                             maxCharacters={PostValidationConstants.MAX_CHARACTERS}/>
                     </div>
 
                 </div>
 
                 <div className="row">
                     <div className="col-lg">
-                        <button type="button" className="btn btn-info btn-sm pane-post-button">Post</button>
-                        <small className="pane-post-left-chars">150</small>
+
+                        <button type="button"
+                                className="btn btn-info btn-sm pane-post-button"
+                                onClick={this.onCreate}>
+                            Post
+                        </button>
+
+                        <small className="pane-post-left-chars">{this.state.leftCharacters}</small>
                     </div>
                 </div>
             </form>
--- iChop - Front-End/front-end/src/constants/validation.constants.js
+++ iChop - Front-End/front-end/src/constants/validation.constants.js
@@ -2,11 +2,17 @@ const ValidationConstants = {
     USER: {
         MAX_STATUS_MESSAGE_CHARACTERS: 16,
         MAX_ABOUT_YOU_CHARACTERS: 250,
+    },
+
+    POST: {
+        MAX_CHARACTERS: 150,
     }
 };
 
 const UserValidationConstants = ValidationConstants.USER;
+const PostValidationConstants = ValidationConstants.POST;
 
 export {
-    UserValidationConstants
+    UserValidationConstants,
+    PostValidationConstants
 };
