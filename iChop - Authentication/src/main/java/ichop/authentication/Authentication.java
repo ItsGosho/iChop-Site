0e1432b5278aea74827c291d1b291734ae7fbc05
Georgi Peev <itsgosho2@gmail.com>
2019-10-17 20:47:29 +0300

    Up

--- iChop - Authentication/src/main/java/ichop/authentication/Authentication.java
+++ iChop - Authentication/src/main/java/ichop/authentication/Authentication.java
@@ -2,6 +2,7 @@ package ichop.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 public class Authentication {
