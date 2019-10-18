2acc33862cf7356578689353dd241eafd5047af2
Georgi Peev <itsgosho2@gmail.com>
2019-10-18 11:06:06 +0300

    Added validation configuration to the threads service

--- Microservices/iChop - Threads Service/src/main/java/ichop/threads/domain/entities/BaseEntity.java
+++ Microservices/iChop - Threads Service/src/main/java/ichop/threads/domain/entities/BaseEntity.java
@@ -2,13 +2,14 @@ package ichop.threads.domain.entities;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 
 @Getter
 @Setter
 public abstract class BaseEntity {
 
-    @Indexed(unique = true)
+    @Id
     private String id;
 
 }
