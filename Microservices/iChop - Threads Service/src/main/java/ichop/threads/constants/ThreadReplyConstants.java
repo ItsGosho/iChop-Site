7e45502991d7d2cf6043432b139ec27fb7bdeb07
itsgosho <itsgosho2@gmail.com>
2019-10-19 01:42:27 +0300

    Added final keyword to the ThreadReplyConstants in the Thread Services

--- Microservices/iChop - Threads Service/src/main/java/ichop/threads/constants/ThreadReplyConstants.java
+++ Microservices/iChop - Threads Service/src/main/java/ichop/threads/constants/ThreadReplyConstants.java
@@ -1,6 +1,6 @@
 package ichop.threads.constants;
 
-public class ThreadReplyConstants {
+public final class ThreadReplyConstants {
 
     public static final String THREAD_CREATED_SUCCESSFUL = "Thread created successful!";
     public static final String THREAD_RETRIEVED_SUCCESSFUL = "Thread retrieved successful!";
