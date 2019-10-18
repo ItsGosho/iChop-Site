9e358143bae9be02462eed07532d24d5a3f34598
itsgosho <itsgosho2@gmail.com>
2019-10-18 23:05:48 +0300

    Added three repository methods for the token ,for finding by id,token and expiration date before (for expired tokens)

--- Microservices/iChop - Tokens Service/src/main/java/ichop/tokens/repositories/PasswordTokenRepository.java
+++ Microservices/iChop - Tokens Service/src/main/java/ichop/tokens/repositories/PasswordTokenRepository.java
@@ -4,6 +4,9 @@ import ichop.tokens.domain.entities.PasswordToken;
 import ichop.tokens.domain.models.service.PasswordTokenServiceModel;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface PasswordTokenRepository extends TokenRepository<PasswordToken> {
+
 }
--- Microservices/iChop - Tokens Service/src/main/java/ichop/tokens/repositories/TokenRepository.java
+++ Microservices/iChop - Tokens Service/src/main/java/ichop/tokens/repositories/TokenRepository.java
@@ -4,6 +4,15 @@ import ichop.tokens.common.domain.BaseEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import java.time.LocalDateTime;
+
 @NoRepositoryBean
-public interface TokenRepository<E extends BaseEntity> extends MongoRepository<E,String> {
+public interface TokenRepository<E extends BaseEntity> extends MongoRepository<E, String> {
+
+    E findByUserId(String userId);
+
+    E findByToken(String token);
+
+    E findByExpirationDateBefore(LocalDateTime date);
+
 }
