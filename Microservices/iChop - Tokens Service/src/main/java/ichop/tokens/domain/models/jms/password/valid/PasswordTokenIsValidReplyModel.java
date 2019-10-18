4d1d5b0dad14454466f6fa5b4e7faa4c7334578f
itsgosho <itsgosho2@gmail.com>
2019-10-19 00:33:22 +0300

    Added is valid request/reply models for the password token

---
+++ Microservices/iChop - Tokens Service/src/main/java/ichop/tokens/domain/models/jms/password/valid/PasswordTokenIsValidReplyModel.java
@@ -0,0 +1,16 @@
+package ichop.tokens.domain.models.jms.password.valid;
+
+import ichop.tokens.common.domain.BaseReplyModel;
+import ichop.tokens.common.domain.BaseRequestModel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class PasswordTokenIsValidReplyModel extends BaseReplyModel {
+
+    private boolean isValid;
+
+}
