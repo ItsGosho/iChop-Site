529824d2a002541c0f9e1877e284e767eeaa864f
itsgosho <itsgosho2@gmail.com>
2019-10-19 00:31:40 +0300

    Added password token request and reply models

--- Microservices/iChop - Tokens Service/src/main/java/ichop/tokens/domain/models/jms/password/create/PasswordTokenCreateReplyModel.java
+++ Microservices/iChop - Tokens Service/src/main/java/ichop/tokens/domain/models/jms/password/create/PasswordTokenCreateReplyModel.java
@@ -1,4 +1,18 @@
 package ichop.tokens.domain.models.jms.password.create;
 
-public class PasswordTokenCreateReplyModel {
+import ichop.tokens.common.domain.BaseReplyModel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class PasswordTokenCreateReplyModel extends BaseReplyModel {
+
+    private String id;
+    private String userId;
+    private String token;
+    private LocalDateTime expirationDate;
+
 }
