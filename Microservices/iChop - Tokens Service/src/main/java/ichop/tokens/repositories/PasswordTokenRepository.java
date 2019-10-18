4055c2026175f464adebb83e656fff37b846f9fd
itsgosho <itsgosho2@gmail.com>
2019-10-18 23:16:45 +0300

    Added the method calls for the repository to the TokenServices

--- Microservices/iChop - Tokens Service/src/main/java/ichop/tokens/repositories/PasswordTokenRepository.java
+++ Microservices/iChop - Tokens Service/src/main/java/ichop/tokens/repositories/PasswordTokenRepository.java
@@ -1,10 +1,7 @@
 package ichop.tokens.repositories;
 
 import ichop.tokens.domain.entities.PasswordToken;
-import ichop.tokens.domain.models.service.PasswordTokenServiceModel;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDateTime;
 
 @Repository
 public interface PasswordTokenRepository extends TokenRepository<PasswordToken> {
--- Microservices/iChop - Tokens Service/src/main/java/ichop/tokens/services/AbstractTokenServices.java
+++ Microservices/iChop - Tokens Service/src/main/java/ichop/tokens/services/AbstractTokenServices.java
@@ -6,12 +6,36 @@ import ichop.tokens.domain.entities.Token;
 import ichop.tokens.domain.models.service.TokenServiceModel;
 import ichop.tokens.repositories.TokenRepository;
 
+import java.time.LocalDateTime;
+
 public abstract class AbstractTokenServices
         <E extends Token, S extends TokenServiceModel, R extends TokenRepository<E>>
         extends AbstractBaseService<E, S, R>
         implements TokenServices<S> {
 
+    private Class<S> serviceModelClass;
+
     public AbstractTokenServices(ObjectMapper objectMapper, R repository) {
         super(objectMapper, repository);
+
+    }
+
+    protected S findByUserId(String userId) {
+        E token = super.repository.findByUserId(userId);
+        return this.toServiceModel(token);
+    }
+
+    protected S findByToken(String tokeen) {
+        E token = super.repository.findByToken(tokeen);
+        return this.toServiceModel(token);
+    }
+
+    protected S findByExpirationDateBefore(LocalDateTime date) {
+        E token = super.repository.findByExpirationDateBefore(date);
+        return this.toServiceModel(token);
+    }
+
+    private S toServiceModel(E e) {
+        return e != null ? super.objectMapper.convertValue(e, super.serviceModelClass) : null;
     }
 }
