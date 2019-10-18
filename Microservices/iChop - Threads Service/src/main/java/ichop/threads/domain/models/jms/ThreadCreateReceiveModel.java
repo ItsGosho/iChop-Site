197c691980c1dfd13c051c5579ce4e86410d6522
Georgi Peev <itsgosho2@gmail.com>
2019-10-18 11:44:46 +0300

    added the ThreadCreateReceiveModel

---
+++ Microservices/iChop - Threads Service/src/main/java/ichop/threads/domain/models/jms/ThreadCreateReceiveModel.java
@@ -0,0 +1,26 @@
+package ichop.threads.domain.models.jms;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class ThreadCreateReceiveModel extends BaseReceiveModel {
+
+    @NotNull
+    @NotEmpty
+    @Length(min = 3,max = 50)
+    private String title;
+
+    @NotNull
+    @NotEmpty
+    private String content;
+
+    @NotNull
+    private String userId;
+
+}
