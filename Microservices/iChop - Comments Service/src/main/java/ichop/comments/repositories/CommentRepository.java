54205e8139c46460e0b604ff635f0c6fc14b52dc
Georgi Peev <itsgosho2@gmail.com>
2019-10-20 17:17:12 +0300

    Added count query for total comments of user in the comment repository

--- Microservices/iChop - Comments Service/src/main/java/ichop/comments/repositories/CommentRepository.java
+++ Microservices/iChop - Comments Service/src/main/java/ichop/comments/repositories/CommentRepository.java
@@ -2,8 +2,13 @@ package ichop.comments.repositories;
 
 import ichop.comments.domain.entities.Comment;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface CommentRepository<E extends Comment> extends MongoRepository<E,String> {
+
+    @Query(value = "{'creatorId': ?0}", count = true)
+    Long getTotalCreatorComments(String creatorId);
+
 }
--- Microservices/iChop - Comments Service/src/main/java/ichop/comments/repositories/ThreadCommentRepository.java
+++ Microservices/iChop - Comments Service/src/main/java/ichop/comments/repositories/ThreadCommentRepository.java
@@ -1,8 +1,11 @@
 package ichop.comments.repositories;
 
 import ichop.comments.domain.entities.ThreadComment;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ThreadCommentRepository extends CommentRepository<ThreadComment> {
+
+
 }
