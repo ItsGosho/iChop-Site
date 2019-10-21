3f1501ef98ab7afc5aa9bb4c95c9f106e8576410
Georgi Peev <itsgosho2@gmail.com>
2019-10-21 20:37:15 +0300

    Enabled scheduling in the tokens service

--- Microservices/iChop - Tokens Service/src/main/java/ichop/tokens/Tokens.java
+++ Microservices/iChop - Tokens Service/src/main/java/ichop/tokens/Tokens.java
@@ -2,8 +2,10 @@ package ichop.tokens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Tokens {
     public static void main(String[] args) {
         SpringApplication.run(Tokens.class, args);
