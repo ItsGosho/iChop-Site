f8e846d24b9ee4da4d1e9f26b5f34e66999fc49b
Georgi Peev <itsgosho2@gmail.com>
2019-10-22 17:32:40 +0300

    Added the reports service from the crt

---
+++ Microservices/iChop - Reports Service/src/test/java/configurations/constants/ConditionalsConditionConstants.java
@@ -0,0 +1,10 @@
+package configurations.constants;
+
+import static ichop.CHANGE.constants.ConditionalConstants.ARTEMIS_CONFIGURATION;
+
+public class ConditionalsConditionConstants {
+
+    public static final String ENABLE_ARTEMIS_CONFIGURATION = ARTEMIS_CONFIGURATION + "=true";
+    public static final String DISABLE_ARTEMIS_CONFIGURATION = ARTEMIS_CONFIGURATION + "=false";
+
+}
