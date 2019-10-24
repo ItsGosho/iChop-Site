0612f348903e402f783d5d9468c8e582d2365389
itsgosho <itsgosho2@gmail.com>
2019-10-24 23:08:03 +0300

    Moved the core server properties to the dev file

--- iChop - Core/src/main/java/ichop/core/areas/user/web/controllers/UserAuthenticationController.java
+++ iChop - Core/src/main/java/ichop/core/areas/user/web/controllers/UserAuthenticationController.java
@@ -2,39 +2,39 @@ package ichop.core.areas.user.web.controllers;
 
 import ichop.core.areas.user.domain.models.binding.UserRegisterBindingModel;
 import ichop.core.areas.user.services.UserServices;
+import ichop.core.common.validation.ValidationHelper;
 import ichop.core.constants.URLConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.validation.Valid;
 
 @Controller
 @PreAuthorize("isAnonymous()")
 public class UserAuthenticationController {
 
     private final UserServices userServices;
+    private final ValidationHelper validationHelper;
 
     @Autowired
-    public UserAuthenticationController(UserServices userServices) {
+    public UserAuthenticationController(UserServices userServices, ValidationHelper validationHelper) {
         this.userServices = userServices;
+        this.validationHelper = validationHelper;
     }
 
 
     @PostMapping(URLConstants.USER_REGISTER_POST)
-    public ResponseEntity proceedRegistration(@Valid UserRegisterBindingModel userRegisterBindingModel, BindingResult bindingResult) {
+    public ResponseEntity proceedRegistration(UserRegisterBindingModel userRegisterBindingModel) {
 
-        if(bindingResult.hasErrors()) {
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        if (!this.validationHelper.isValid(userRegisterBindingModel)) {
+            return new ResponseEntity<>(this.validationHelper.getValidationError(userRegisterBindingModel), HttpStatus.BAD_REQUEST);
         }
 
         this.userServices.register(userRegisterBindingModel);
         //this.userWebStorageJmsServices.sendUpdateAvatarRequestWithInitialImage(userRegisterBindingModel.getUsername());
-        return new ResponseEntity<>("",HttpStatus.OK);
+        return new ResponseEntity<>("", HttpStatus.OK);
     }
 
 }
