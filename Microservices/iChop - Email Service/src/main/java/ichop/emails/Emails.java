b11e27d9e55e6675ff1ddcc1998b2e35c3ff46f2
Georgi Peev <itsgosho2@gmail.com>
2019-10-25 11:56:42 +0300

    Added the initial email services structure

---
+++ Microservices/iChop - Email Service/src/main/java/ichop/emails/Emails.java
@@ -0,0 +1,11 @@
+package ichop.emails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Emails {
+    public static void main(String[] args) {
+        SpringApplication.run(Emails.class, args);
+    }
+}
---
+++ Microservices/iChop - Email Service/src/main/java/ichop/emails/common/helpers/JmsHelper.java
@@ -0,0 +1,19 @@
+package ichop.emails.common.helpers;
+
+import ichop.emails.common.domain.BaseReplyModel;
+import ichop.emails.common.domain.BaseRequestModel;
+
+import javax.jms.Message;
+
+public interface JmsHelper {
+
+    <S extends BaseReplyModel,R extends BaseRequestModel> S sendAndReceive(String destination, R model, Class<S> clazz);
+
+    <S extends BaseRequestModel> void send(String destination, S model);
+
+    <S extends BaseReplyModel> void replySuccessful(Message message, S model, String msg);
+
+    <R> R getResultModel(Message message, Class<R> clazz);
+
+    <S extends BaseRequestModel> void replyValidationError(Message message, S receiveModel);
+}
