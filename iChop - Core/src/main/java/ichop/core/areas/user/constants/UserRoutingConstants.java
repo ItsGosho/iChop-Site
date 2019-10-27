866e27f7a148661724cc5d8bce94a78a2796c53a
itsgosho <itsgosho2@gmail.com>
2019-10-27 23:09:42 +0200

    Removed the user prefix from the user urls in the core

--- iChop - Core/src/main/java/ichop/core/areas/user/constants/UserRoutingConstants.java
+++ iChop - Core/src/main/java/ichop/core/areas/user/constants/UserRoutingConstants.java
@@ -11,32 +11,32 @@ public final class UserRoutingConstants {
 
     public static final String IS_FOLLOWING = "/user/is/following"; /*EVERYONE*/
     public static final String EXISTS = "/user/exists"; /*EVERYONE*/
+    public static final String ALL = "/users/all"; /*ADMIN++*/
 
     /*TODO:*/
 
     //UserController ------------------------------------------------------------------------------------------
 
-    public static final String USER_PROFILE = "/user/{username}/profile"; /*EVERYONE*/
-    public static final String USER_ALL = "/user/all"; /*ADMIN++*/
+    public static final String PROFILE = "/user/{username}/profile"; /*EVERYONE*/
 
     //UserFollowController ------------------------------------------------------------------------------------
 
-    public static final String USER_FOLLOW = "/user/{username}/follow"; /*USER++*/
-    public static final String USER_UNFOLLOW = "/user/{username}/unfollow"; /*USER++*/
-    public static final String USER_ALL_FOLLOWERS = "/api/user/{username}/all/followers"; /*EVERYONE*/
-    public static final String USER_ALL_FOLLOWINGS = "/api/user/{username}/all/followings"; /*EVERYONE*/
+    public static final String FOLLOW = "/user/{username}/follow"; /*USER++*/
+    public static final String UNFOLLOW = "/user/{username}/unfollow"; /*USER++*/
+    public static final String ALL_FOLLOWERS = "/user/{username}/all/followers"; /*EVERYONE*/
+    public static final String ALL_FOLLOWINGS = "/user/{username}/all/followings"; /*EVERYONE*/
 
     //UserControlController -----------------------------------------------------------------------------------
 
-    public static final String USER_CONTROL_BASE = "/user/{username}/control"; /*ADMIN++*/
-    public static final String USER_CONTROL_ROLE_MANAGEMENT = "/user/{username}/control/role-management"; /*ADMIN++*/
-    public static final String USER_CONTROL_ROLE_MANAGEMENT_PROMOTE_USER = "/user/{username}/control/role-management/promote"; /*ADMIN++*/
+    public static final String CONTROL_BASE = "/user/{username}/control"; /*ADMIN++*/
+    public static final String CONTROL_ROLE_MANAGEMENT = "/user/{username}/control/role-management"; /*ADMIN++*/
+    public static final String CONTROL_ROLE_MANAGEMENT_PROMOTE_USER = "/user/{username}/control/role-management/promote"; /*ADMIN++*/
 
 
     //UserMyProfileController ---------------------------------------------------------------------------------
 
-    public static final String USER_PROFILE_OPTIONS_INFORMATION = "/user/my-profile/options/information"; /*Only LOGGED-IN USER IN HIS PROFILE*/
-    public static final String USER_PROFILE_OPTIONS_CHANGE_PASSWORD = "/user/my-profile/options/change-password"; /*Only LOGGED-IN USER IN HIS PROFILE*/
-    public static final String USER_PROFILE_OPTIONS_MINECRAFT = "/user/my-profile/options/minecraft"; /*Only LOGGED-IN USER IN HIS PROFILE*/
+    public static final String PROFILE_OPTIONS_INFORMATION = "/user/my-profile/options/information"; /*Only LOGGED-IN USER IN HIS PROFILE*/
+    public static final String PROFILE_OPTIONS_CHANGE_PASSWORD = "/user/my-profile/options/change-password"; /*Only LOGGED-IN USER IN HIS PROFILE*/
+    public static final String PROFILE_OPTIONS_MINECRAFT = "/user/my-profile/options/minecraft"; /*Only LOGGED-IN USER IN HIS PROFILE*/
 
 }
