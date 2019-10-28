ceb30419a7fd59178240f99f05ba565d75c58ecf
itsgosho <itsgosho2@gmail.com>
2019-10-28 09:05:11 +0200

    Added the new user destinations in the props and the requester for them in the core service

--- iChop - Core/src/main/java/ichop/core/areas/user/requester/UserRequester.java
+++ iChop - Core/src/main/java/ichop/core/areas/user/requester/UserRequester.java
@@ -1,5 +1,11 @@
 package ichop.core.areas.user.requester;
 
+import ichop.core.areas.user.models.jms.password.change.UserChangePasswordByTokenReply;
+import ichop.core.areas.user.models.jms.password.change.UserChangePasswordByTokenRequest;
+import ichop.core.areas.user.models.jms.password.change.UserChangePasswordReply;
+import ichop.core.areas.user.models.jms.password.change.UserChangePasswordRequest;
+import ichop.core.areas.user.models.jms.password.forgotten.UserForgottenPasswordReply;
+import ichop.core.areas.user.models.jms.password.forgotten.UserForgottenPasswordRequest;
 import ichop.core.areas.user.models.jms.register.UserRegisterReply;
 import ichop.core.areas.user.models.jms.register.UserRegisterRequest;
 import ichop.core.areas.user.models.jms.retrieve.UserFindByEmailReply;
@@ -8,4 +14,10 @@ public interface UserRequester {
     UserFindByEmailReply findByEmail(String email);
 
     UserRegisterReply register(UserRegisterRequest request);
+
+    UserChangePasswordReply changePassword(UserChangePasswordRequest request);
+
+    UserChangePasswordByTokenReply changePasswordByToken(UserChangePasswordByTokenRequest request);
+
+    UserForgottenPasswordReply forgottenPassword(UserForgottenPasswordRequest request);
 }
--- iChop - Core/src/main/java/ichop/core/areas/user/requester/UserRequesterImp.java
+++ iChop - Core/src/main/java/ichop/core/areas/user/requester/UserRequesterImp.java
@@ -1,5 +1,11 @@
 package ichop.core.areas.user.requester;
 
+import ichop.core.areas.user.models.jms.password.change.UserChangePasswordByTokenReply;
+import ichop.core.areas.user.models.jms.password.change.UserChangePasswordByTokenRequest;
+import ichop.core.areas.user.models.jms.password.change.UserChangePasswordReply;
+import ichop.core.areas.user.models.jms.password.change.UserChangePasswordRequest;
+import ichop.core.areas.user.models.jms.password.forgotten.UserForgottenPasswordReply;
+import ichop.core.areas.user.models.jms.password.forgotten.UserForgottenPasswordRequest;
 import ichop.core.areas.user.models.jms.register.UserRegisterReply;
 import ichop.core.areas.user.models.jms.register.UserRegisterRequest;
 import ichop.core.areas.user.models.jms.retrieve.UserFindByEmailReply;
@@ -17,14 +23,25 @@ public class UserRequesterImp implements UserRequester {
     private String findByEmailDestination;
     private String registerDestination;
 
+    private String changePasswordDestination;
+    private String changePasswordByTokenDestination;
+    private String forgottenPasswordDestination;
+
     @Autowired
     public UserRequesterImp(JmsHelper jmsHelper,
                             @Value("${artemis.queue.users.find.by.email}") String findByEmailDestination,
-                            @Value("${artemis.queue.users.authentication.register}") String registerDestination) {
+                            @Value("${artemis.queue.users.authentication.register}") String registerDestination,
+                            @Value("${artemis.queue.users.password.change}") String changePasswordDestination,
+                            @Value("${artemis.queue.users.password.change.by.token}") String changePasswordByTokenDestination,
+                            @Value("${artemis.queue.users.forgotten.password}") String forgottenPasswordDestination) {
         this.jmsHelper = jmsHelper;
 
         this.findByEmailDestination = findByEmailDestination;
         this.registerDestination = registerDestination;
+
+        this.changePasswordDestination = changePasswordDestination;
+        this.changePasswordByTokenDestination = changePasswordByTokenDestination;
+        this.forgottenPasswordDestination = forgottenPasswordDestination;
     }
 
     @Override
@@ -37,6 +54,21 @@ public class UserRequesterImp implements UserRequester {
     @Override
     public UserRegisterReply register(UserRegisterRequest request) {
         return this.jmsHelper.sendAndReceive(this.registerDestination, request, UserRegisterReply.class);
+    }
+
+    @Override
+    public UserChangePasswordReply changePassword(UserChangePasswordRequest request) {
+        return this.jmsHelper.sendAndReceive(this.changePasswordDestination, request, UserChangePasswordReply.class);
+    }
+
+    @Override
+    public UserChangePasswordByTokenReply changePasswordByToken(UserChangePasswordByTokenRequest request) {
+        return this.jmsHelper.sendAndReceive(this.changePasswordByTokenDestination, request, UserChangePasswordByTokenReply.class);
+    }
+
+    @Override
+    public UserForgottenPasswordReply forgottenPassword(UserForgottenPasswordRequest request) {
+        return this.jmsHelper.sendAndReceive(this.forgottenPasswordDestination, request, UserForgottenPasswordReply.class);
     }
 
 }
