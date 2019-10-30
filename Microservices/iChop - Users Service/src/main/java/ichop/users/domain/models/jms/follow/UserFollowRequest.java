39ba127ae83d7bc7052c5a70785a5ce96216b3a8
itsgosho <itsgosho2@gmail.com>
2019-10-30 23:11:46 +0200

    Added validation to the follow ,to check if the user is trying to follow himself

--- Microservices/iChop - Users Service/src/main/java/ichop/users/domain/models/jms/follow/UserFollowRequest.java
+++ Microservices/iChop - Users Service/src/main/java/ichop/users/domain/models/jms/follow/UserFollowRequest.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 @Setter
 @SpELValidation(value = "@userServicesImp.existsByUsername(#this.username) == true", message = "User not found!")
 @SpELValidation(value = "@userServicesImp.existsByUsername(#this.followUsername) == true", message = "User to follow not found!")
+@SpELValidation(value = "#this.username != #this.followUsername", message = "You canno't follow yourself!")
 @SpELValidation(value = "@userFollowServicesImp.isFollowed(#this.username,#this.followUsername) == false", message = "Already following!")
 public class UserFollowRequest extends BaseRequestModel {
 
