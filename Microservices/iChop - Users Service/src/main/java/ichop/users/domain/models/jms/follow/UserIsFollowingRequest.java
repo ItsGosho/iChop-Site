8667af89af9064d8f41a16e11558070645c57cb6
itsgosho <itsgosho2@gmail.com>
2019-10-30 23:57:28 +0200

    Removed the two SpEL validations from the IsFollowing request model in the user service

--- Microservices/iChop - Users Service/src/main/java/ichop/users/domain/models/jms/follow/UserIsFollowingRequest.java
+++ Microservices/iChop - Users Service/src/main/java/ichop/users/domain/models/jms/follow/UserIsFollowingRequest.java
@@ -1,14 +1,13 @@
 package ichop.users.domain.models.jms.follow;
 
 import ichop.users.common.domain.BaseRequestModel;
-import ichop.users.common.validators.SpELValidation;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@SpELValidation(value = "@userServicesImp.existsByUsername(#this.username) == true",message = "User not found!")
-@SpELValidation(value = "@userServicesImp.existsByUsername(#this.followingUsername) == true",message = "Following not found!")
+/*@SpELValidation(value = "@userServicesImp.existsByUsername(#this.username) == true",message = "User not found!")
+@SpELValidation(value = "@userServicesImp.existsByUsername(#this.followingUsername) == true",message = "Following not found!")*/
 public class UserIsFollowingRequest extends BaseRequestModel {
 
     private String username;
