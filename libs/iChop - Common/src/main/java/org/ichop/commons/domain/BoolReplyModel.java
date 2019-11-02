7d992236f7e6443ce66ab23e78843c977ccf1937
itsgosho <itsgosho2@gmail.com>
2019-11-03 00:51:59 +0200

    Added BoolReplyModel to the common service

---
+++ libs/iChop - Common/src/main/java/org/ichop/commons/domain/BoolReplyModel.java
@@ -0,0 +1,12 @@
+package org.ichop.commons.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoolReplyModel extends BaseReplyModel {
+
+    private boolean result;
+
+}
