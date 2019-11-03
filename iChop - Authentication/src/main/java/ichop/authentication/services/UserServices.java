f9625d11d9bc384080be4f2a1118559462b674ee
itsgosho <itsgosho2@gmail.com>
2019-11-03 22:00:47 +0200

    Removed the iChop - Authentication Service

--- iChop - Authentication/src/main/java/ichop/authentication/services/UserServices.java
+++
@@ -1,7 +0,0 @@
-package ichop.authentication.services;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserServices {
-}
