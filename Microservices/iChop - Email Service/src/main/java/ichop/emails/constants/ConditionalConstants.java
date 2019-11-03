dea1d6f15dd11e9b75cedaa27519ed0ca8f0d113
itsgosho <itsgosho2@gmail.com>
2019-11-03 16:13:37 +0200

    Finished refactoring the emails ervice to be compliant with the new request/reply structure

--- Microservices/iChop - Email Service/src/main/java/ichop/emails/constants/ConditionalConstants.java
+++ Microservices/iChop - Email Service/src/main/java/ichop/emails/constants/ConditionalConstants.java
@@ -2,7 +2,6 @@ package ichop.emails.constants;
 
 public final class ConditionalConstants {
 
-    public static final String ARTEMIS_CONFIGURATION = "ArtemisConfiguration";
     public static final String EMAIL_CONFIGURATION = "EmailConfiguration";
 
 }
--- Microservices/iChop - Email Service/src/main/java/ichop/emails/listeners/EmailListeners.java
+++ Microservices/iChop - Email Service/src/main/java/ichop/emails/listeners/EmailListeners.java
@@ -1,22 +1,22 @@
 package ichop.emails.listeners;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import ichop.emails.common.aop.JmsValidate;
-import ichop.emails.common.helpers.BaseListener;
-import ichop.emails.common.helpers.JmsHelper;
 import ichop.emails.constants.SubjectConstants;
-import ichop.emails.domain.models.jms.EmailReply;
 import ichop.emails.domain.models.jms.EmailResetPasswordRequest;
 import ichop.emails.helpers.FreemakerHelper;
 import ichop.emails.helpers.JavaMailHelper;
+import org.ichop.commons.aop.JmsValidate;
+import org.ichop.commons.domain.EmptyReply;
+import org.ichop.commons.helpers.BaseListener;
+import org.ichop.commons.helpers.JmsHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import javax.jms.Message;
 
-import static ichop.emails.common.constants.JmsFactories.QUEUE;
 import static ichop.emails.constants.EmailReplyConstants.EMAIL_SENT_SUCCESSFUL;
+import static org.ichop.commons.constants.JmsFactories.QUEUE;
 
 @Component
 public class EmailListeners extends BaseListener {
@@ -36,15 +36,15 @@ public class EmailListeners extends BaseListener {
     @JmsValidate(model = EmailResetPasswordRequest.class)
     @JmsListener(destination = "${artemis.queue.emails.reset_password}", containerFactory = QUEUE)
     public void create(Message message) {
-        EmailResetPasswordRequest requestModel = this.jmsHelper.getResultModel(message, EmailResetPasswordRequest.class);
+        EmailResetPasswordRequest requestModel = this.jmsHelper.toModel(message, EmailResetPasswordRequest.class);
 
         try {
             String html = this.freemakerHelper.prepareResetPasswordView(requestModel.getExpirationDate(), requestModel.getToken());
             this.javaMailHelper.send(requestModel.getTo(), SubjectConstants.RESET_PASSWORD, html);
 
-            this.jmsHelper.replySuccessful(message, new EmailReply(), EMAIL_SENT_SUCCESSFUL);
+            this.jmsHelper.replySuccessful(message, new EmptyReply(), EMAIL_SENT_SUCCESSFUL);
         } catch (Exception e) {
-            this.jmsHelper.replyFailed(message, new EmailReply(), e.getMessage());
+            this.jmsHelper.replyError(message, new EmptyReply(), e.getMessage());
         }
     }
 
