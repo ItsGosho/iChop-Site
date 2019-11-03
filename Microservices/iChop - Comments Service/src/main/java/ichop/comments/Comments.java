27dccd7051542e86cd9909efb8d599d817529b7c
itsgosho <itsgosho2@gmail.com>
2019-11-03 22:13:52 +0200

    Removed the commons folder and added component scan and the commons lib to the comments service

--- Microservices/iChop - Comments Service/src/main/java/ichop/comments/Comments.java
+++ Microservices/iChop - Comments Service/src/main/java/ichop/comments/Comments.java
@@ -2,8 +2,10 @@ package ichop.comments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"ichop.comments","org.ichop.commons"})
 public class Comments {
     public static void main(String[] args) {
         SpringApplication.run(Comments.class, args);
