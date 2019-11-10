0e9e8594a2e8f5a30fb50913d2ea3a0e034cce0b
itsgosho <itsgosho2@gmail.com>
2019-11-10 23:30:07 +0200

    Added the initial UserProfile skeleton in components did mount ,which  will fill the information about the user in the store

--- iChop - Front-End/front-end/src/components/user/profile/UserProfile.js
+++ iChop - Front-End/front-end/src/components/user/profile/UserProfile.js
@@ -5,6 +5,9 @@ import UserProfileCentralContent from "./UserProfileCentralContent";
 import {withRouter} from "react-router-dom";
 import UserServices from "../../../services/user.services";
 import NotificationHelper from "../../../helpers/notification.helper";
+import {compose} from "redux";
+import {connect} from "react-redux";
+import userProfileInfoDispatchers from "../../../redux/dispatchers/user.profile.info.dispatchers";
 
 class UserProfile extends Component {
 
@@ -12,34 +15,48 @@ class UserProfile extends Component {
         super(props);
 
         this.state = {
-            user: undefined,
-            isLoading: true
+            isLoading: true,
+            isFound: false,
         }
     }
 
     async componentDidMount() {
         let {username} = this.props.match.params;
         let user = await UserServices.findByUsername(username);
+        let followings = undefined;
+        let followers = undefined;
+        let posts = [];
+        let information = undefined;
+        let minecraftUUID = undefined;
+        let minecraftAccountName = undefined;
+        let totalLikes = 0;
+        let totalDislikes = 0;
 
         if (!user) {
             NotificationHelper.showErrorNotification(`User wasn't found`)
         } else {
-            this.setState({user: user})
+            this.props.setUser(user);
+            this.props.setFollow(followings, followers);
+            this.props.setPosts(posts);
+            this.props.setInformation(information);
+            this.props.setMinecraft(minecraftUUID, minecraftAccountName);
+            this.props.setReaction(totalLikes, totalDislikes);
         }
 
-        this.setState({isLoading: false})
+        this.setState({isLoading: false});
+        this.setState({isFound: user !== undefined});
     }
 
     render() {
-        let {user, isLoading} = this.state;
+        let {isFound, isLoading} = this.state;
 
         return (
             <Fragment>
-                {user !== undefined && !isLoading ? (
+                {!isLoading && isFound ? (
                     <div className="container container-user-profile">
                         <div className="row">
-                            <UserProfileLeftSideInformation user={user}/>
-                            <UserProfileCentralContent user={user}/>
+                            <UserProfileLeftSideInformation/>
+                            <UserProfileCentralContent/>
                         </div>
                     </div>
                 ) : null}
@@ -49,4 +66,12 @@ class UserProfile extends Component {
 
 }
 
-export default withRouter(UserProfile);
+let mapState = (states) => {
+    return {userProfileInfo: states.userProfileInfo}
+};
+
+export default withRouter(
+    compose(
+        connect(mapState, userProfileInfoDispatchers),
+    )(UserProfile)
+)
