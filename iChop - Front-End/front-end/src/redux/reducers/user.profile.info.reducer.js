be81f639b72c4293ab93c374bd005d39ea7cacac
itsgosho <itsgosho2@gmail.com>
2019-11-10 22:29:56 +0200

    Added the reducer for the user profile with all of its actions

--- iChop - Front-End/front-end/src/redux/reducers/user.profile.info.reducer.js
+++ iChop - Front-End/front-end/src/redux/reducers/user.profile.info.reducer.js
@@ -34,8 +34,8 @@ let userProfileInfoReducer = (state = initialState, action) => {
 
     switch (action.type) {
 
-        case Actions.SET_AUTHENTICATED_USER_INFO:
-            let {id, username, email, authority, registrationDate, lastOnline, location} = action.payload.user;
+        case Actions.SET_USER_PROFILE_USER:
+            let {id, username, email, authority, registrationDate, lastOnline, location} = action.payload;
 
             return Object.assign({}, state, {
                 id,
@@ -47,8 +47,35 @@ let userProfileInfoReducer = (state = initialState, action) => {
                 location
             });
 
-        case Actions.REMOVE_AUTHENTICATED_USER_INFO:
-            return {...initialState};
+        case Actions.SET_USER_PROFILE_FOLLOW:
+            let {followings, followers} = action.payload;
+
+            return Object.assign({}, state, {
+                followings,
+                followers,
+                totalFollowing: followings.length,
+                totalFollowers: followers.length,
+            });
+
+        case Actions.SET_USER_PROFILE_POSTS:
+            let {posts} = action.payload;
+
+            return Object.assign({}, state, {posts});
+
+        case Actions.SET_USER_PROFILE_INFORMATION:
+            let {aboutYou, birthday} = action.payload;
+
+            return Object.assign({}, state, {aboutYou, birthday});
+
+        case Actions.SET_USER_PROFILE_MINECRAFT:
+            let {minecraftUUID, minecraftAccountName} = action.payload;
+
+            return Object.assign({}, state, {minecraftUUID, minecraftAccountName});
+
+        case Actions.SET_USER_PROFILE_REACTIONS:
+            let {totalLikes, totalDislikes} = action.payload;
+
+            return Object.assign({}, state, {totalLikes, totalDislikes});
 
         default:
             return state;
