218a8d989b5d295596013930feda6b021d5e9970
itsgosho <itsgosho2@gmail.com>
2019-11-23 19:35:26 +0200

    The folder for the footer`s etc has been renamed to components and her components have been refactored so much ,to be functional

---
+++ iChop - Front-End/front-end/src/components/footer/partials/FooterAuthenticatedLinks.jsx
@@ -0,0 +1,23 @@
+import React, {Fragment} from 'react';
+import RoutingURLs from "../../../constants/routing/routing.constants";
+import LinkIconLi from "../../other/LinkIconLi";
+import {connect} from "react-redux";
+import formsDispatchers from "../../../redux/dispatchers/forms.dispatchers";
+import PrefixURLs from "../../../constants/routing/prefix.routing.constants";
+
+const FooterAuthenticatedLinks = (props) => {
+    let {username: authenticatedUsername} = props.authenticatedUserInfo;
+    let profileUrl = RoutingURLs.USER.PROFILE.VIEW.replace(':username', authenticatedUsername);
+
+    return (
+        <Fragment>
+
+            <LinkIconLi url={profileUrl} icon={'👤'} text={'Profile'}/>
+            <LinkIconLi url={PrefixURLs.OPTIONS_PREFIX} icon={'⚙'} text={'Options'}/>
+            <LinkIconLi url={RoutingURLs.AUTHENTICATION.LOGOUT} icon={'🚪'} text={'Logout'}/>
+
+        </Fragment>
+    )
+};
+
+export default connect((state) => ({...state}), formsDispatchers)(FooterAuthenticatedLinks);
