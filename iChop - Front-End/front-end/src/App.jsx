6649e60f4754bbe362318eba673566743f3b2ce6
itsgosho <itsgosho2@gmail.com>
2019-12-05 23:40:06 +0200

    The App component will now be a arrow function.

--- iChop - Front-End/front-end/src/App.jsx
+++ iChop - Front-End/front-end/src/App.jsx
@@ -17,55 +17,42 @@ import UserControl from "./components/user/control/UserControl";
 import Reports from "./components/report/Reports";
 import PlayerLinkAccount from "./components/player/PlayerLinkAccount";
 
-function App() {
+let App = () => (
+    <Fragment>
+        <Navbar/>
+
+        <ReactNotification/>
+
+        <div style={{'marginTop': '75px'}}>
+            <Switch>
+                <Route exact path={RoutingURLs.HOME} render={() => (<ThreadsAll/>)}/>
+
+                <Route exact path={RoutingURLs.AUTHENTICATION.TOKEN.RESET_PASSWORD}
+                       render={() => (<UserChangePasswordByToken/>)}/>
+
+                <Route exact path={RoutingURLs.USER.PROFILE.VIEW(':username')}
+                       render={(props) => (<UserProfile key={props.match.params.username} {...props}/>)}/>
+
+                <Route path={PrefixURLs.USER_CONTROL_PREFIX(':username')} render={() => (<UserControl/>)}/>
+
+                <Route path={PrefixURLs.OPTIONS_PREFIX} render={() => (<UserOptions/>)}/>
+
+                <Route path={RoutingURLs.REPORT.ALL} render={() => (<Reports/>)}/>
 
 
-    return (
-        <Fragment>
-            <Navbar/>
+                <Route exact path={RoutingURLs.THREAD.READ(':id')}
+                       render={(props) => (<ThreadRead key={props.match.params.id} {...props}/>)}/>
 
-            <ReactNotification/>
+                <Route exact path={RoutingURLs.THREAD.CREATE} render={() => (<ThreadCreate/>)}/>
 
-            <div style={{'marginTop': '75px'}}>
-                <Switch>
-                    <Route exact path={RoutingURLs.HOME} render={() => (<ThreadsAll/>)}/>
+                <Route exact path={RoutingURLs.PLAYER.ACCOUNT.LINK} render={() => (<PlayerLinkAccount/>)}/>
 
-                    <Route exact path={RoutingURLs.AUTHENTICATION.TOKEN.RESET_PASSWORD}
-                           render={() => (<UserChangePasswordByToken/>)}/>
+                <Route exact path={RoutingURLs.AUTHENTICATION.LOGOUT} render={() => (<Logout/>)}/>
+            </Switch>
+        </div>
 
-                    <Route exact path={RoutingURLs.USER.PROFILE.VIEW(':username')}
-                           render={(props) => (<UserProfile key={props.match.params.username} {...props}/>)}/>
-
-                    <Route path={PrefixURLs.USER_CONTROL_PREFIX(':username')} render={() => (<UserControl/>)}/>
-
-                    <Route path={PrefixURLs.OPTIONS_PREFIX} render={() => (<UserOptions/>)}/>
-
-                  <Route path={RoutingURLs.REPORT.ALL} render={() => (<Reports/>)}/>
-
-
-                    <Route exact path={RoutingURLs.THREAD.READ(':id')}
-                           render={(props) => (<ThreadRead key={props.match.params.id} {...props}/>)}/>
-
-                    <Route exact path={RoutingURLs.THREAD.CREATE} render={() => (<ThreadCreate/>)}/>
-
-                    <Route exact path={RoutingURLs.PLAYER.ACCOUNT.LINK} render={() => (<PlayerLinkAccount/>)}/>
-
-                    <Route exact path={RoutingURLs.AUTHENTICATION.LOGOUT} render={() => (<Logout/>)}/>
-                </Switch>
-            </div>
-
-            {/*<div style={{'marginTop': '75px'}}>
-
-                <Switch>
-                    <Route exact path={RoutingURLs.USER.CONTROL.INFORMATION} render={({match})=>(<UserControl route={match}/>)}/>
-                    <Route exact path={RoutingURLs.USER.CONTROL.ROLE} render={({match})=>(<UserControl route={match}/>)}/>
-                </Switch>
-
-            </div>*/}
-
-            <Footer/>
-        </Fragment>
-    );
-}
+        <Footer/>
+    </Fragment>
+);
 
 export default App;
